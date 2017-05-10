d37b78ab8a210ed67cc70ab8d0f250c9f1d0b8b5
Robert Fromont <robert@fromont.net.nz>
2017-05-10 19:01:33 -0300

    Add unit tests for TextGrid serialization.

--- java/nzilbb/praat/test/TestTextGridDeserializer.java
+++ java/nzilbb/praat/test/TestTextGridDeserializer.java
@@ -29,12 +29,20 @@ import java.util.LinkedHashMap;
 import java.util.SortedSet;
 import java.util.LinkedHashSet;
 import java.util.Iterator;
+import java.util.Vector;
+import java.util.List;
 import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
 import java.net.URL;
 import nzilbb.configure.ParameterSet;
 import nzilbb.configure.Parameter;
+import nzilbb.editpath.MinimumEditPath;
+import nzilbb.editpath.EditStep;
 import nzilbb.ag.*;
 import nzilbb.ag.serialize.util.NamedStream;
+import nzilbb.ag.serialize.util.Utility;
+import nzilbb.ag.serialize.json.JSONSerialization;
 import nzilbb.praat.*;
 
 public class TestTextGridDeserializer
@@ -1279,8 +1287,6 @@ public class TestTextGridDeserializer
 
       assertEquals("orphan phone", "<p:>", phones[13].getLabel());
       assertNull("no phone parent", phones[13].getParent());
-
-
    }
 
    @Test public void performance() 
@@ -1331,6 +1337,120 @@ public class TestTextGridDeserializer
       assertTrue("Deserialization too slow:\n" + deserializer.getTimers().toString(),
 		 1500 > deserializer.getTimers().getTotals().get("deserialize"));
    }
+
+   @Test public void serialize_utterance_word() 
+      throws Exception
+   {
+      Schema schema = new Schema(
+	 "who", "turn", "utterance", "word",
+	 new Layer("who", "Participants", 0, true, true, true),
+	 new Layer("comment", "Comment", 2, true, false, true),
+	 new Layer("noise", "Noise", 2, true, false, true),
+	 new Layer("turn", "Speaker turns", 2, true, false, false, "who", true),
+	 new Layer("utterance", "Utterances", 2, true, false, true, "turn", true),
+	 new Layer("word", "Words", 2, true, false, false, "turn", true),
+	 new Layer("phone", "Phones", 2, true, true, true, "word", true),
+	 new Layer("lexical", "Lexical", 0, true, false, false, "word", true),
+	 new Layer("pronounce", "Pronounce", 0, false, false, true, "word", true));
+      File dir = getDir();
+      // access file
+      NamedStream[] jsonStreams = { new NamedStream(new File(dir, "serialize_utterance_word.json")) };
+      
+      // deserialize graph from JSON
+      JSONSerialization json = new JSONSerialization();
+      json.configure(json.configure(new ParameterSet(), schema), schema);
+      json.setParameters(json.load(jsonStreams, schema));
+      Graph[] graphs = json.deserialize();
+
+      // create serializer
+      TextGridDeserializer serializer = new TextGridDeserializer();
+      
+      // general configuration
+      ParameterSet configuration = serializer.configure(new ParameterSet(), schema);
+      //for (Parameter p : configuration.values()) System.out.println("config " + p.getName() + " = " + p.getValue());
+      assertEquals(6, serializer.configure(configuration, schema).size());
+
+      String[] needLayers = serializer.getRequiredLayers();
+      assertEquals(7, needLayers.length);
+      assertEquals("who", needLayers[0]);
+      assertEquals("turn", needLayers[1]);
+      assertEquals("word", needLayers[2]);
+      assertEquals("lexical", needLayers[3]);
+      assertEquals("pronounce", needLayers[4]);
+      assertEquals("comment", needLayers[5]);
+      assertEquals("noise", needLayers[6]);
+	 
+      // serialize
+      NamedStream[] streams = serializer.serialize(graphs);
+      streams[0].save(dir);
+
+      // test using diff
+      String differences = diff(new File(dir, "expected_serialize_utterance_word.TextGrid"),
+				new File(dir, "serialize_utterance_word.TextGrid"));
+      if (differences != null) fail(differences);	 
+   }
+
+   
+   /**
+    * Diffs two files.
+    * @param expected
+    * @param actual
+    * @return null if the files are the same, and a String describing differences if not.
+    */
+   public String diff(File expected, File actual)
+   {
+      StringBuffer d = new StringBuffer();
+      
+      try
+      {
+	 // compare with what we expected
+	 Vector<String> actualLines = new Vector<String>();
+	 BufferedReader reader = new BufferedReader(new FileReader(actual));
+	 String line = reader.readLine();
+	 while (line != null)
+	 {
+	    actualLines.add(line);
+	    line = reader.readLine();
+	 }
+	 Vector<String> expectedLines = new Vector<String>();
+	 reader = new BufferedReader(new FileReader(expected));
+	 line = reader.readLine();
+	 while (line != null)
+	 {
+	    expectedLines.add(line);
+	    line = reader.readLine();
+	 }
+	 MinimumEditPath<String> comparator = new MinimumEditPath<String>();
+	 List<EditStep<String>> path = comparator.minimumEditPath(expectedLines, actualLines);
+	 for (EditStep<String> step : path)
+	 {
+	    switch (step.getOperation())
+	    {
+	       case CHANGE:
+		  d.append("\n"+expected.getPath()+":"+(step.getFromIndex()+1)+": Expected:\n" 
+			   + step.getFrom() 
+			   + "\n"+actual.getPath()+":"+(step.getToIndex()+1)+": Found:\n" + step.getTo());
+		  break;
+	       case DELETE:
+		  d.append("\n"+expected.getPath()+":"+(step.getFromIndex()+1)+": Deleted:\n" 
+			   + step.getFrom()
+			   + "\n"+actual.getPath()+":"+(step.getToIndex()+1)+": Missing");
+		  break;
+	       case INSERT:
+		  d.append("\n"+expected.getPath()+":"+(step.getFromIndex()+1)+": Missing" 
+			   + "\n"+actual.getPath()+":"+(step.getToIndex()+1)+": Inserted:\n" 
+			   + step.getTo());
+		  break;
+	    }
+	 } // next step
+      }
+      catch(Exception exception)
+      {
+	 d.append("\n" + exception);
+      }
+      if (d.length() > 0) return d.toString();
+      return null;
+   } // end of diff()
 
 
    /**
