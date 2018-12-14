a6969990329999740b57dce05256f07fd53d55b8
Robert Fromont <robert@fromont.net.nz>
2018-12-14 19:31:27 -0300

    API version 20181214.1844

--- java/nzilbb/ag/Constants.java
+++ java/nzilbb/ag/Constants.java
@@ -30,7 +30,7 @@ public class Constants
 {
 
    /** API version */
-   public static final String VERSION = "20180903.1831";
+   public static final String VERSION = "20181214.1844";
    
    /** Standardized possible value for anchor offset or annotation label confidence - no confidence. Value is 0. */
    public static final int CONFIDENCE_NONE = 0;
