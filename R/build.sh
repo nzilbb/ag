49fac62538668fabe947f81166a8976d433b9c67
Robert Fromont <robert@fromont.net.nz>
2019-03-08 19:24:28 -0300

    Add build script

---
+++ R/build.sh
@@ -0,0 +1,13 @@
+echo delete editor backup files...
+find . -name "*~" -exec rm \{\} \;
+
+echo build nzilbb.labbcat ...
+R CMD build nzilbb.labbcat
+
+echo check nzilbb.labbcat ...
+R CMD check --as-cran nzilbb.labbcat_*.tar.gz
+
+echo copy package to bin
+mv nzilbb.labbcat_*.tar.gz ../bin/nzilbb.labbcat.tar.gz
+
+echo finished.
