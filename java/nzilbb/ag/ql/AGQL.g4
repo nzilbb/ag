5e1a1bcad3a47432097c1906faa5ebc538b2e875
Robert Fromont <robert@fromont.net.nz>
2019-06-12 15:27:07 -0300

    Prototype for AGQL parser.

---
+++ java/nzilbb/ag/ql/AGQL.g4
@@ -0,0 +1,181 @@
+/**
+ * Annotation Graph Query Language grammar.
+ * Copyright 2015-2019 New Zealand Institute of Language, Brain and Behaviour, 
+ * University of Canterbury
+ * Written by Robert Fromont - robert.fromont@canterbury.ac.nz
+ *
+ *    This file is part of nzilbb.ag.
+ *
+ *    nzilbb.ag is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    nzilbb.ag is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with nzilbb.ag; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+grammar AGQL;
+
+@header {
+package nzilbb.ag.ql;
+}
+
+/* Parser rules: */
+
+query : booleanExpression EOF;
+
+booleanExpression
+  : booleanExpression logicalOperator predicate        # CompositeExpression
+  | predicate                                          # PredicateExpression
+  ;
+
+predicate
+  : operand comparisonOperator operand                 # ComparisonPredicate
+  | attribute                                          # AttributePredicate
+  | method                                             # MethodPredicate
+  ;
+
+operand
+  : graphIdExpression                                  # GraphIdOperand
+  | corpusNameExpression                               # CorpusNameOperand
+  | episodeNameExpression                              # EpisodeNameOperand
+  | whoExpression                                      # WhoOperand
+  | anchorIdExpression                                 # AnchorIdOperand
+  | anchorOffsetExpression                             # AnchorOffsetOperand
+  | labelExpression                                    # LabelOperand
+  | idExpression                                       # IdOperand
+  | listExpression                                     # ListOperand
+  | listLengthExpression                               # ListLengthOperand
+  | ANNOTATOR                                          # AnnotatorOperand
+  | WHEN                                               # WhenOperand
+  | attribute                                          # AttributeOperand
+  | method                                             # MethodOperand
+  | atom                                               # AtomOperand
+  ;
+
+attribute : atom DOT IDENTIFIER;
+
+graphIdExpression
+  : MY OPEN_PAREN GRAPH_LITERAL CLOSE_PAREN DOT LABEL
+  | MY OPEN_PAREN GRAPH_LITERAL CLOSE_PAREN DOT ID
+  | GRAPH DOT LABEL
+  | GRAPH DOT ID
+  ;
+
+corpusNameExpression : MY OPEN_PAREN CORPUS_LITERAL CLOSE_PAREN DOT LABEL ;
+episodeNameExpression : MY OPEN_PAREN EPISODE_LITERAL CLOSE_PAREN DOT LABEL ;
+whoExpression
+  : LABELS OPEN_PAREN WHO_LITERAL CLOSE_PAREN          # WhoLabelsExpression
+  | MY OPEN_PAREN WHO_LITERAL CLOSE_PAREN DOT LABEL    # WhoLabelExpression
+  ;
+anchorIdExpression
+  : START DOT ID                                       # StartIdExpression
+  | END DOT ID                                         # EndIdExpression
+  ;
+anchorOffsetExpression
+  : START DOT OFFSET                                   # StartOffsetExpression
+  | END DOT OFFSET                                     # EndOffsetExpression
+  ;
+labelExpression : MY OPEN_PAREN stringLiteral CLOSE_PAREN DOT LABEL ;
+idExpression : MY OPEN_PAREN stringLiteral CLOSE_PAREN DOT ID ;
+listExpression : LIST OPEN_PAREN stringLiteral CLOSE_PAREN ;
+listLengthExpression : LIST OPEN_PAREN stringLiteral CLOSE_PAREN DOT LENGTH ;
+    
+method
+  : atom DOT IDENTIFIER OPEN_PAREN CLOSE_PAREN         # MethodNoArgs 
+  | atom DOT IDENTIFIER OPEN_PAREN arglist CLOSE_PAREN # MethodArgs 
+  ;
+
+arglist : operand (COMMA atom)* ;
+
+atom
+  : literal                   # LiteralAtom
+  | IDENTIFIER                # IdentifierAtom
+  ;
+
+literal
+  : stringLiteral
+  | INTEGER_LITERAL
+  | NUMBER_LITERAL
+  ;
+
+stringLiteral
+  : quotedString=DOUBLE_QUOTED_STRING
+  | quotedString=SINGLE_QUOTED_STRING
+  ;
+
+comparisonOperator
+  : EQ
+  | NE
+  | MATCHES
+  | NOT_MATCHES
+  | LT
+  | GT
+  | LTE
+  | GTE
+  | IN
+  | NOT_IN
+  ;
+
+logicalOperator
+  : AND
+  | OR
+  ;
+
+/* Lexer rules: */
+
+/* special layers */
+WHO_LITERAL           : '"who"' | '\'who\'' ;
+GRAPH_LITERAL         : '"graph"' | '\'graph\'' ;
+CORPUS_LITERAL        : '"corpus"' | '\'corpus\'' ;
+EPISODE_LITERAL       : '"episode"' | '\'episode\'' ;
+
+/* special variables */
+GRAPH                 : 'graph' ;
+
+/* attributes */
+ID                    : 'id' ;
+LABEL                 : 'label' ;
+START                 : 'start' ;
+END                   : 'end' ;
+OFFSET                : 'offset' ;
+ANNOTATOR             : 'annotator' ;
+WHEN                  : 'when' ;
+LENGTH                : 'length' ;
+
+/* methods */
+MY                    : 'my' ;
+LIST                  : 'list' ;
+LABELS                : 'labels' ;
+
+/* other stuff */
+DOT                   : '.' ;
+COMMA                 : ',' ;
+IDENTIFIER            : [a-zA-Z][a-zA-Z0-9_]* ;
+AND                   : ' AND ' ;
+OR                    : ' OR ' ;
+EQ                    : '=' ;
+NE                    : '<>' ;
+MATCHES               : ' MATCHES ' ;
+NOT_MATCHES           : ' NOT MATCHES ' ;
+LT                    : '<' ;
+GT                    : '>' ;
+LTE                   : '<=' ;
+GTE                   : '>=' ;
+IN                    : ' IN ' ;
+NOT_IN                : ' NOT IN ' ;
+OPEN_PAREN            : '(' ;
+CLOSE_PAREN           : ')' ;
+DOUBLE_QUOTED_STRING  : '"' (~'"')+ '"';
+SINGLE_QUOTED_STRING  : '\'' (~'\'')+ '\'';
+INTEGER_LITERAL       : '-'? [0-9]+ ;
+NUMBER_LITERAL        : '-'? [0-9]+ '.' [0-9]+ ; 
+
+/* ignore white space */
+WS : [ \n\t\r]+ -> channel(HIDDEN);
---
+++ java/nzilbb/ag/ql/test/TestAGQLListener.java
@@ -0,0 +1,455 @@
+//
+// Copyright 2019 New Zealand Institute of Language, Brain and Behaviour, 
+// University of Canterbury
+// Written by Robert Fromont - robert.fromont@canterbury.ac.nz
+//
+//    This file is part of nzilbb.ag.
+//
+//    nzilbb.ag is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    nzilbb.ag is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with nzilbb.ag; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+package nzilbb.ag.ql.test;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import nzilbb.ag.ql.*;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+public class TestAGQLListener
+{
+  @Test public void graphId() 
+  {
+    final StringBuffer parse = new StringBuffer();
+    final StringBuffer error = new StringBuffer();
+    AGQLListener listener = new AGQLBaseListener() {
+        @Override public void exitGraphIdExpression(AGQLParser.GraphIdExpressionContext ctx)
+        {
+          parse.append(ctx.getText());
+        }
+        @Override public void visitErrorNode(ErrorNode node)
+        {
+          error.append(node.getText());
+        }
+      };
+
+    AGQLLexer lexer = new AGQLLexer(
+      CharStreams.fromString("my('graph').label = \"something\""));
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    AGQLParser parser = new AGQLParser(tokens);
+    AGQLParser.QueryContext tree = parser.query();
+
+    ParseTreeWalker.DEFAULT.walk(listener, tree);
+    assertTrue("No errors: " + error.toString(), error.length() == 0);
+    assertEquals("Parse structure: " + parse,
+                 "my('graph').label", parse.toString());
+
+    parse.setLength(0);
+    lexer.setInputStream(CharStreams.fromString("my(\"graph\").id = 'something'"));
+    tokens = new CommonTokenStream(lexer);
+    parser = new AGQLParser(tokens);
+    tree = parser.query();
+    ParseTreeWalker.DEFAULT.walk(listener, tree);
+    assertEquals("Parse structure: " + parse,
+                 "my(\"graph\").id", parse.toString());
+    
+    parse.setLength(0);
+    lexer.setInputStream(CharStreams.fromString("graph.id = 'something'"));
+    tokens = new CommonTokenStream(lexer);
+   parser = new AGQLParser(tokens);
+    tree = parser.query();
+    ParseTreeWalker.DEFAULT.walk(listener, tree);
+    assertEquals("Parse structure: " + parse,
+                 "graph.id", parse.toString());
+    
+    parse.setLength(0);
+    lexer.setInputStream(CharStreams.fromString("graph.label = 'something'"));
+    tokens = new CommonTokenStream(lexer);
+    parser = new AGQLParser(tokens);
+    tree = parser.query();
+    ParseTreeWalker.DEFAULT.walk(listener, tree);
+    assertEquals("Parse structure: " + parse,
+                 "graph.label", parse.toString());
+    
+  }
+
+  @Test public void corpusName() 
+  {
+    final StringBuffer parse = new StringBuffer();
+    final StringBuffer error = new StringBuffer();
+    AGQLListener listener = new AGQLBaseListener() {
+        @Override public void exitCorpusNameExpression(AGQLParser.CorpusNameExpressionContext ctx)
+        {
+          parse.append(ctx.getText());
+        }
+        @Override public void visitErrorNode(ErrorNode node)
+        {
+          error.append(node.getText());
+        }
+      };
+
+    AGQLLexer lexer = new AGQLLexer(
+      CharStreams.fromString("my('corpus').label = \"something\""));
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    AGQLParser parser = new AGQLParser(tokens);
+    AGQLParser.QueryContext tree = parser.query();
+
+    ParseTreeWalker.DEFAULT.walk(listener, tree);
+    assertTrue("No errors: " + error.toString(), error.length() == 0);
+    assertEquals("Parse structure: " + parse,
+                 "my('corpus').label", parse.toString());
+
+  }
+
+  @Test public void episodeName() 
+  {
+    final StringBuffer parse = new StringBuffer();
+    final StringBuffer error = new StringBuffer();
+    AGQLListener listener = new AGQLBaseListener() {
+        @Override public void exitEpisodeNameExpression(AGQLParser.EpisodeNameExpressionContext ctx)
+        {
+          parse.append(ctx.getText());
+        }
+        @Override public void visitErrorNode(ErrorNode node)
+        {
+          error.append(node.getText());
+        }
+      };
+
+    AGQLLexer lexer = new AGQLLexer(
+      CharStreams.fromString("my('episode').label = \"something\""));
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    AGQLParser parser = new AGQLParser(tokens);
+    AGQLParser.QueryContext tree = parser.query();
+
+    ParseTreeWalker.DEFAULT.walk(listener, tree);
+    assertTrue("No errors: " + error.toString(), error.length() == 0);
+    assertEquals("Parse structure: " + parse,
+                 "my('episode').label", parse.toString());
+
+  }
+
+  @Test public void whoLabel() 
+  {
+    final StringBuffer parse = new StringBuffer();
+    final StringBuffer error = new StringBuffer();
+    AGQLListener listener = new AGQLBaseListener() {
+        @Override public void exitWhoLabelExpression(AGQLParser.WhoLabelExpressionContext ctx)
+        {
+          parse.append(ctx.getText());
+        }
+        @Override public void visitErrorNode(ErrorNode node)
+        {
+          error.append(node.getText());
+        }
+      };
+
+    AGQLLexer lexer = new AGQLLexer(
+      CharStreams.fromString("my('who').label = \"something\""));
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    AGQLParser parser = new AGQLParser(tokens);
+    AGQLParser.QueryContext tree = parser.query();
+
+    ParseTreeWalker.DEFAULT.walk(listener, tree);
+    assertTrue("No errors: " + error.toString(), error.length() == 0);
+    assertEquals("Parse structure: " + parse,
+                 "my('who').label", parse.toString());
+
+  }
+
+  @Test public void whoLabels() 
+  {
+    final StringBuffer parse = new StringBuffer();
+    final StringBuffer error = new StringBuffer();
+    AGQLListener listener = new AGQLBaseListener() {
+        @Override public void exitWhoLabelsExpression(AGQLParser.WhoLabelsExpressionContext ctx)
+        {
+          parse.append(ctx.getText());
+        }
+        @Override public void visitErrorNode(ErrorNode node)
+        {
+          error.append(node.getText());
+        }
+      };
+
+    AGQLLexer lexer = new AGQLLexer(
+      CharStreams.fromString("\"something\" IN labels('who')"));
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    AGQLParser parser = new AGQLParser(tokens);
+    AGQLParser.QueryContext tree = parser.query();
+
+    ParseTreeWalker.DEFAULT.walk(listener, tree);
+    assertTrue("No errors: " + error.toString(), error.length() == 0);
+    assertEquals("Parse structure: " + parse,
+                 "labels('who')", parse.toString());
+
+  }
+
+  @Test public void anchorId() 
+  {
+    final StringBuffer parse = new StringBuffer();
+    final StringBuffer error = new StringBuffer();
+    AGQLListener listener = new AGQLBaseListener() {
+        @Override public void exitAnchorIdOperand(AGQLParser.AnchorIdOperandContext ctx)
+        {
+          if (parse.length() > 0) parse.append(" ");
+          parse.append(ctx.getText());
+        }
+        @Override public void visitErrorNode(ErrorNode node)
+        {
+          error.append(node.getText());
+        }
+      };
+
+    AGQLLexer lexer = new AGQLLexer(
+      CharStreams.fromString("start.id = end.id"));
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    AGQLParser parser = new AGQLParser(tokens);
+    AGQLParser.QueryContext tree = parser.query();
+
+    ParseTreeWalker.DEFAULT.walk(listener, tree);
+    assertTrue("No errors: " + error.toString(), error.length() == 0);
+    assertEquals("Parse structure: " + parse,
+                 "start.id end.id", parse.toString());
+
+  }
+  
+  @Test public void anchorOffset() 
+  {
+    final StringBuffer parse = new StringBuffer();
+    final StringBuffer error = new StringBuffer();
+    AGQLListener listener = new AGQLBaseListener() {
+        // @Override public void exitEveryRule(ParserRuleContext ctx)
+        // {
+        //   System.out.println(ctx.getClass().getSimpleName() + ": " + ctx.getText());
+        // }
+        @Override public void exitAnchorOffsetOperand(AGQLParser.AnchorOffsetOperandContext ctx)
+        {
+          if (parse.length() > 0) parse.append(" ");
+          parse.append(ctx.getText());
+        }
+        @Override public void visitErrorNode(ErrorNode node)
+        {
+          // System.out.println(node.getText());
+          error.append(node.getText());
+        }
+      };
+
+    AGQLLexer lexer = new AGQLLexer(
+      CharStreams.fromString("start.offset = end.offset"));
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    AGQLParser parser = new AGQLParser(tokens);
+    AGQLParser.QueryContext tree = parser.query();
+
+    ParseTreeWalker.DEFAULT.walk(listener, tree);
+    assertTrue("No errors: " + error.toString(), error.length() == 0);
+    assertEquals("Parse structure: " + parse,
+                 "start.offset end.offset", parse.toString());
+
+  }
+  
+  @Test public void label() 
+  {
+    final StringBuffer parse = new StringBuffer();
+    final StringBuffer error = new StringBuffer();
+    AGQLListener listener = new AGQLBaseListener() {
+        @Override public void exitLabelExpression(AGQLParser.LabelExpressionContext ctx)
+        {
+          parse.append(ctx.stringLiteral().quotedString.getText());
+        }
+        @Override public void visitErrorNode(ErrorNode node)
+        {
+          error.append(node.getText());
+        }
+      };
+
+    AGQLLexer lexer = new AGQLLexer(
+      CharStreams.fromString("my('transcript').label = 'something'"));
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    AGQLParser parser = new AGQLParser(tokens);
+    AGQLParser.QueryContext tree = parser.query();
+
+    ParseTreeWalker.DEFAULT.walk(listener, tree);
+    assertTrue("No errors: " + error.toString(), error.length() == 0);
+    assertEquals("Parse structure: " + parse,
+                 "'transcript'", parse.toString());
+
+  }
+  
+  @Test public void id() 
+  {
+    final StringBuffer parse = new StringBuffer();
+    final StringBuffer error = new StringBuffer();
+    AGQLListener listener = new AGQLBaseListener() {
+        @Override public void exitIdExpression(AGQLParser.IdExpressionContext ctx)
+        {
+          parse.append(ctx.stringLiteral().quotedString.getText());
+        }
+        @Override public void visitErrorNode(ErrorNode node)
+        {
+          error.append(node.getText());
+        }
+      };
+
+    AGQLLexer lexer = new AGQLLexer(
+      CharStreams.fromString("my('transcript').id = 'something'"));
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    AGQLParser parser = new AGQLParser(tokens);
+    AGQLParser.QueryContext tree = parser.query();
+
+    ParseTreeWalker.DEFAULT.walk(listener, tree);
+    assertTrue("No errors: " + error.toString(), error.length() == 0);
+    assertEquals("Parse structure: " + parse,
+                 "'transcript'", parse.toString());
+
+  }
+  
+  @Test public void list() 
+  {
+    final StringBuffer parse = new StringBuffer();
+    final StringBuffer error = new StringBuffer();
+    AGQLListener listener = new AGQLBaseListener() {
+        // @Override public void exitEveryRule(ParserRuleContext ctx)
+        // {
+        //   System.out.println(ctx.getClass().getSimpleName() + ": " + ctx.getText());
+        // }
+        @Override public void exitListExpression(AGQLParser.ListExpressionContext ctx)
+        {
+          parse.append(ctx.stringLiteral().quotedString.getText());
+        }
+        @Override public void visitErrorNode(ErrorNode node)
+        {
+          // System.out.println(node.getText());
+          error.append(node.getText());
+        }
+      };
+
+    AGQLLexer lexer = new AGQLLexer(
+      CharStreams.fromString("list('transcript') = 'something'"));
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    AGQLParser parser = new AGQLParser(tokens);
+    AGQLParser.QueryContext tree = parser.query();
+
+    ParseTreeWalker.DEFAULT.walk(listener, tree);
+    assertTrue("No errors: " + error.toString(), error.length() == 0);
+    assertEquals("Parse structure: " + parse,
+                 "'transcript'", parse.toString());
+
+  }
+
+  @Test public void listLength() 
+  {
+    final StringBuffer parse = new StringBuffer();
+    final StringBuffer error = new StringBuffer();
+    AGQLListener listener = new AGQLBaseListener() {
+        // @Override public void exitEveryRule(ParserRuleContext ctx)
+        // {
+        //   System.out.println(ctx.getClass().getSimpleName() + ": " + ctx.getText());
+        // }
+        @Override public void exitListLengthExpression(AGQLParser.ListLengthExpressionContext ctx)
+        {
+          parse.append(ctx.stringLiteral().quotedString.getText());
+        }
+        @Override public void visitErrorNode(ErrorNode node)
+        {
+          // System.out.println(node.getText());
+          error.append(node.getText());
+        }
+      };
+
+    AGQLLexer lexer = new AGQLLexer(
+      CharStreams.fromString("list('transcript').length = 0"));
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    AGQLParser parser = new AGQLParser(tokens);
+    AGQLParser.QueryContext tree = parser.query();
+
+    ParseTreeWalker.DEFAULT.walk(listener, tree);
+    assertTrue("No errors: " + error.toString(), error.length() == 0);
+    assertEquals("Parse structure: " + parse,
+                 "'transcript'", parse.toString());
+
+  }
+  
+  @Test public void annotator() 
+  {
+    final StringBuffer parse = new StringBuffer();
+    final StringBuffer error = new StringBuffer();
+    AGQLListener listener = new AGQLBaseListener() {
+        @Override public void exitAnnotatorOperand(AGQLParser.AnnotatorOperandContext ctx)
+        {
+          parse.append(ctx.getText());
+        }
+        @Override public void visitErrorNode(ErrorNode node)
+        {
+          error.append(node.getText());
+        }
+      };
+
+    AGQLLexer lexer = new AGQLLexer(
+      CharStreams.fromString("annotator = 'somebody'"));
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    AGQLParser parser = new AGQLParser(tokens);
+    AGQLParser.QueryContext tree = parser.query();
+
+    ParseTreeWalker.DEFAULT.walk(listener, tree);
+    assertTrue("No errors: " + error.toString(), error.length() == 0);
+    assertEquals("Parse structure: " + parse,
+                 "annotator", parse.toString());
+
+  }
+
+  @Test public void when() 
+  {
+    final StringBuffer parse = new StringBuffer();
+    final StringBuffer error = new StringBuffer();
+    AGQLListener listener = new AGQLBaseListener() {
+        // @Override public void exitEveryRule(ParserRuleContext ctx)
+        // {
+        //   System.out.println(ctx.getClass().getSimpleName() + ": " + ctx.getText());
+        // }
+        @Override public void exitWhenOperand(AGQLParser.WhenOperandContext ctx)
+        {
+          parse.append(ctx.getText());
+        }
+        @Override public void visitErrorNode(ErrorNode node)
+        {
+          // System.out.println("ERROR: " + node.getText());
+          error.append(node.getText());
+        }
+      };
+
+    AGQLLexer lexer = new AGQLLexer(
+      CharStreams.fromString("when < '2018-01-01 00:00:00'"));
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    AGQLParser parser = new AGQLParser(tokens);
+    AGQLParser.QueryContext tree = parser.query();
+
+    ParseTreeWalker.DEFAULT.walk(listener, tree);
+    assertTrue("No errors: " + error.toString(), error.length() == 0);
+    assertEquals("Parse structure: " + parse,
+                 "when", parse.toString());
+
+  }
+
+  public static void main(String args[]) 
+  {
+    org.junit.runner.JUnitCore.main("nzilbb.ag.ql.test.TestAGQLListener");
+  }
+
+}
