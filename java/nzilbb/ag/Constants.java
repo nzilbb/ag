4c9e0b5ac10efcf5caf4accef363fea8ef953df4
Robert Fromont <robert@fromont.net.nz>
2019-06-18 13:55:48 -0300

    Version 20190618.1348 - AGQL for matching graphs and participants.

--- java/nzilbb/ag/Constants.java
+++ java/nzilbb/ag/Constants.java
@@ -30,7 +30,7 @@ public class Constants
 {
 
    /** API version */
-   public static final String VERSION = "20190613.1753";
+   public static final String VERSION = "20190618.1348";
    
    /** Standardized possible value for anchor offset or annotation label confidence - no confidence. Value is 0. */
    public static final int CONFIDENCE_NONE = 0;
