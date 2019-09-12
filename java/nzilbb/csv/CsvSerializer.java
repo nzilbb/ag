430f54fdbb928c082e89e675a6bf011129cc998e
Robert Fromont <robert@fromont.net.nz>
2019-09-12 16:42:50 -0300

    Implement CsvSerializer

---
+++ java/nzilbb/csv/CsvSerializer.java
@@ -0,0 +1,344 @@
+//
+// Copyright 2019 New Zealand Institute of Language, Brain and Behaviour, 
+// University of Canterbury
+// Written by Robert Fromont - robert.fromont@canterbury.ac.nz
+//
+//    This file is part of nzilbb.ag.
+//
+//    nzilbb.ag is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    nzilbb.ag is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with nzilbb.ag; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+package nzilbb.csv;
+
+import java.util.Vector;
+import java.util.LinkedHashMap;
+import java.util.HashMap;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.List;
+import java.util.Arrays;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+import nzilbb.ag.*;
+import nzilbb.ag.serialize.ISerializer;
+import nzilbb.ag.serialize.ISeriesSerializer;
+import nzilbb.ag.serialize.SerializationDescriptor;
+import nzilbb.ag.serialize.SerializationParametersMissingException;
+import nzilbb.ag.serialize.SerializerNotConfiguredException;
+import nzilbb.ag.serialize.SerializationException;
+import nzilbb.util.TempFileInputStream;
+import nzilbb.ag.serialize.util.NamedStream;
+import nzilbb.ag.serialize.util.Utility;
+import nzilbb.util.ISeries;
+import nzilbb.util.ArraySeries;
+import nzilbb.util.ISeriesConsumer;
+import nzilbb.configure.Parameter;
+import nzilbb.configure.ParameterSet;
+import org.apache.commons.csv.*;
+
+/**
+ * Converter that generates CSV files from annotation graphs.
+ * @author Robert Fromont robert@fromont.net.nz
+ */
+public class CsvSerializer
+   implements ISerializer
+{
+   // Attributes:
+   
+   /** Static format so we don't keep creating and destroying one */
+   private static DecimalFormat fmt = new DecimalFormat(
+      // force the locale to something with . as the decimal separator
+      "0.000", new DecimalFormatSymbols(Locale.UK));
+   
+   protected Vector<String> warnings;
+   /**
+    * Returns any warnings that may have arisen during the last execution of {@link #deserialize()}.
+    * <p>{@link ISerializer} and {@link IDeserializer} method.
+    * @return A possibly empty list of warnings.
+    */
+   public String[] getWarnings()
+   {
+      return warnings.toArray(new String[0]);
+   }
+   
+   /**
+    * Layer schema.
+    * @see #getSchema()
+    * @see #setSchema(Schema)
+    */
+   protected Schema schema;
+   /**
+    * Getter for {@link #schema}: Layer schema.
+    * @return Layer schema.
+    */
+   public Schema getSchema() { return schema; }
+   /**
+    * Setter for {@link #schema}: Layer schema.
+    * @param newSchema Layer schema.
+    */
+   public CsvSerializer setSchema(Schema newSchema) { schema = newSchema; return this; }
+
+   /**
+    * Participant information layer.
+    * @see #getParticipantLayer()
+    * @see #setParticipantLayer(Layer)
+    */
+   protected Layer participantLayer;
+   /**
+    * Getter for {@link #participantLayer}: Participant information layer.
+    * @return Participant information layer.
+    */
+   public Layer getParticipantLayer() { return participantLayer; }
+   /**
+    * Setter for {@link #participantLayer}: Participant information layer.
+    * @param newParticipantLayer Participant information layer.
+    */
+   public CsvSerializer setParticipantLayer(Layer newParticipantLayer) { participantLayer = newParticipantLayer; return this; }
+   
+   /**
+    * Minimum confidence for anchor offsets. Offsets with lower confidence will not be serialized.
+    * @see #getMinimumAnchorConfidence()
+    * @see #setMinimumAnchorConfidence(Integer)
+    */
+   protected Integer minimumAnchorConfidence = Integer.valueOf(Constants.CONFIDENCE_AUTOMATIC);
+   /**
+    * Getter for {@link #minimumAnchorConfidence}: Minimum confidence for anchor offsets. Offsets with lower confidence will not be serialized.
+    * @return Minimum confidence for anchor offsets. Offsets with lower confidence will not be serialized.
+    */
+   public Integer getMinimumAnchorConfidence() { return minimumAnchorConfidence; }
+   /**
+    * Setter for {@link #minimumAnchorConfidence}: Minimum confidence for anchor offsets. Offsets with lower confidence will not be serialized.
+    * @param newMinimumAnchorConfidence Minimum confidence for anchor offsets. Offsets with lower confidence will not be serialized.
+    */
+   public CsvSerializer setMinimumAnchorConfidence(Integer newMinimumAnchorConfidence) { minimumAnchorConfidence = newMinimumAnchorConfidence; return this; }
+
+   // Methods:
+   
+   /**
+    * Default constructor.
+    */
+   public CsvSerializer()
+   {
+   } // end of constructor
+
+   /**
+    * Returns the deserializer's descriptor.
+    * <p>{@link ISerializer} and {@link IDeserializer} method.
+    * @return The deserializer's descriptor
+    */
+   public SerializationDescriptor getDescriptor()
+   {
+      return new SerializationDescriptor(
+	 "Comma Separated Values", "0.1", "text/csv", ".csv", "20190906.1040", getClass().getResource("icon.png"));
+   }
+
+   /**
+    * Sets parameters for deserializer as a whole.  This might include database connection parameters, locations of supporting files, etc.
+    * <p>When the deserializer is installed, this method should be invoked with an empty parameter
+    *  set, to discover what (if any) general configuration is required. If parameters are
+    *  returned, and user interaction is possible, then the user may be presented with an
+    *  interface for setting/confirming these parameters.
+    * <p>{@link ISerializer} and {@link IDeserializer} method.
+    * @param configuration The configuration for the deserializer. 
+    * @param schema The layer schema, definining layers and the way they interrelate.
+    * @return A list of configuration parameters (still) must be set before {@link IDeserializer#setParameters()} can be invoked. If this is an empty list, {@link IDeserializer#setParameters()} can be invoked. If it's not an empty list, this method must be invoked again with the returned parameters' values set.
+    */
+   public ParameterSet configure(ParameterSet configuration, Schema schema)
+   {
+      setSchema(schema);
+      setParticipantLayer(schema.getParticipantLayer());
+      return configuration;
+   }   
+
+   /**
+    * Determines which layers, if any, must be present in the graph that will be serialized.
+    * <p>{@link ISerializer} method.
+    * @return A list of IDs of layers that must be present in the graph that will be serialized.
+    * @throws SerializationParametersMissingException If not all required parameters have a value.
+    */
+   public String[] getRequiredLayers() throws SerializationParametersMissingException
+   {
+      return new String[0];
+   }
+
+   // ISerializer method
+   
+   /**
+    * Serializes the given graphs, generating one or more {@link NamedStream}s.
+    * <p>Many data formats will only yield one stream (e.g. Transcriber transcript or Praat
+    *  textgrid), however there are formats that use multiple files for the same transcript
+    *  (e.g. XWaves, EmuR), which is why this method returns a list. There are formats that
+    *  are capable of storing multiple transcripts in the same file (e.g. AGTK, Transana XML
+    *  export), which is why this method accepts a list.
+    * <p>{@link ISerializer} method.
+    * @param graphs The graphs to serialize.
+    * @param layerIds The IDs of the layers to include, or null for all layers.
+    * @return A list of named streams that contain the serialization in the given format. 
+    * @throws SerializerNotConfiguredException if the object has not been configured.
+    * @throws SerializationException if errors occur during deserialization.
+    */
+   public NamedStream[] serialize(Graph[] graphs, String[] layerIds) 
+      throws SerializerNotConfiguredException, SerializationException
+   {
+      warnings = new Vector<String>();
+      try
+      {
+         String fileName = "annotations";
+         if (graphs.length >= 1)
+         {
+            fileName = graphs[0].getId().replaceAll("\\.[^.]+$","");
+            if (graphs.length > 1) fileName += "-etc";
+         }
+         File csvFile = File.createTempFile(fileName+".",".csv");
+         CSVPrinter csv = new CSVPrinter(
+            new OutputStreamWriter(new FileOutputStream(csvFile), "UTF-8"), CSVFormat.EXCEL);
+         NamedStream csvStream = new NamedStream(
+            new TempFileInputStream(csvFile), fileName+".csv", "text/csv");
+         
+         csv.print("graph");
+         Vector<Layer> attributeLayers = new Vector<Layer>();
+         Vector<Layer> temporalLayers = new Vector<Layer>();
+         for (String layerId : layerIds)
+         {
+            Layer layer = schema.getLayer(layerId);
+            if (layer.getParentId().equals(schema.getRoot().getId())
+                && layer.getAlignment() == Constants.ALIGNMENT_NONE)
+            {
+               attributeLayers.add(layer);
+            }
+            else
+            {
+               temporalLayers.add(layer);
+            }
+         } // next layerId
+
+         // there will be an attribute layer value, but no offsets, for every CSV row
+         for (Layer layer : attributeLayers)
+         {
+            csv.print(layer.getId());
+         }
+
+         // there will be one temporal layer/value per CSV row
+         for (Layer layer : temporalLayers)
+         {
+            csv.print(layer.getId());
+            switch (layer.getAlignment())
+            {
+               case Constants.ALIGNMENT_INSTANT:
+                  csv.print(layer.getId() + " offset");
+                  break;
+               default: // INTERVAL and NONE
+                  csv.print(layer.getId() + " start");
+                  csv.print(layer.getId() + " end");
+                  break;
+            }
+         } // next temporal layer
+         csv.println();
+         for (Graph graph : graphs)
+         {
+            // target one temporal layer at a time
+            for (Layer targetLayer : temporalLayers)
+            {
+               // there's one CSV row per annotation
+               for (Annotation annotation : graph.list(targetLayer.getId()))
+               {
+                  // graph ID
+                  csv.print(graph.getId());
+
+                  // all the attribute layers
+                  for (Layer attributeLayer : attributeLayers)
+                  {
+                     try
+                     {
+                        csv.print(annotation.my(attributeLayer.getId()).getLabel());
+                     }
+                     catch(NullPointerException exception)
+                     {
+                        csv.print("");
+                     }
+                  } // next attribute layer
+
+                  // now iterate through the temporal layers, inserting the label in the
+                  // right column, and adding blanks in the other columns
+                  for (Layer columnLayer : temporalLayers)
+                  {
+                     if (columnLayer == targetLayer)
+                     { // put values in these columns
+                        csv.print(annotation.getLabel());
+                        switch (columnLayer.getAlignment())
+                        {
+                           case Constants.ALIGNMENT_INSTANT:
+                              csv.print(offset(annotation.getStart()));
+                              break;
+                           default: // INTERVAL and NONE
+                              csv.print(offset(annotation.getStart()));
+                              csv.print(offset(annotation.getEnd()));
+                              break;
+                        }
+                     } // columnLayer == targetLayer
+                     else
+                     { // blank columns
+                        csv.print("");
+                        switch (columnLayer.getAlignment())
+                        {
+                           case Constants.ALIGNMENT_INSTANT:
+                              csv.print("");
+                              break;
+                           default: // INTERVAL and NONE
+                              csv.print("");
+                              csv.print("");
+                              break;
+                        }
+                     } // blank columns
+                  } // next temporal layer
+                  csv.println();
+               } // next annotation
+            } // next layer
+         } // next graph
+         csv.flush();
+         csv.close();
+         NamedStream[] streams = { csvStream };
+         return streams;     
+      }
+      catch(Exception exception)
+      {
+         throw new SerializationException(exception);
+      }
+   }
+
+   
+   /**
+    * Returns the anchor's offset, or an empty string.
+    * @param anchor
+    * @return A string representing the offset, or an empty string if the offset is not set, or it's below #minimumAnchorConfidence.
+    */
+   public String offset(Anchor anchor)
+   {
+      if (anchor == null) return "";
+      if (anchor.getOffset() == null) return "";
+      if (minimumAnchorConfidence != null)
+      {
+         if (anchor.getConfidence() == null) return "";
+         if (anchor.getConfidence() < minimumAnchorConfidence) return "";
+      }
+      return anchor.getOffset().toString();
+   } // end of offset()
+
+
+} // end of class CsvSerializer
---
+++ java/nzilbb/csv/test/TestCsvSerializer.java
@@ -0,0 +1,386 @@
+//
+// Copyright 2019 New Zealand Institute of Language, Brain and Behaviour, 
+// University of Canterbury
+// Written by Robert Fromont - robert.fromont@canterbury.ac.nz
+//
+//    This file is part of nzilbb.ag.
+//
+//    nzilbb.ag is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    nzilbb.ag is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with nzilbb.ag; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+package nzilbb.csv.test;
+	      
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.net.URL;
+import nzilbb.configure.ParameterSet;
+import nzilbb.configure.Parameter;
+import nzilbb.editpath.MinimumEditPath;
+import nzilbb.editpath.EditStep;
+import nzilbb.ag.*;
+import nzilbb.ag.serialize.util.NamedStream;
+import nzilbb.csv.*;
+
+public class TestCsvSerializer
+{      
+   @Test public void basicSerialization() 
+      throws Exception
+   {
+      Graph g = createGraph("basicSerialization.trs");
+      
+      // create deserializer
+      CsvSerializer serializer = new CsvSerializer();
+      
+      ParameterSet configuration = serializer.configure(new ParameterSet(), g.getSchema());
+      // for (Parameter p : configuration.values()) System.out.println("config " + p.getName() + " = " + p.getValue());
+      assertEquals(0, serializer.configure(configuration, g.getSchema()).size());
+      
+      // no layers required
+      String[] requiredLayers = serializer.getRequiredLayers();
+      assertEquals(0, requiredLayers.length);
+      
+      // serialize
+      Graph[] graphs = { g };
+      File dir = getDir();
+      String[] layerIds = {"who", "word"};
+      NamedStream[] streams = serializer.serialize(graphs, layerIds);
+      assertEquals(1, streams.length);
+      streams[0].save(dir);
+      
+      File actual = new File(dir, streams[0].getName());
+      String differences = diff(
+         new File(dir, "expected_" + streams[0].getName()),
+         actual);
+      if (differences != null)
+      {
+         fail(differences);
+      }
+      else
+      {
+         actual.delete();
+      }
+   }
+
+   @Test public void ignoreConfidence() 
+      throws Exception
+   {
+      Graph g = createGraph("ignoreConfidence.trs");
+      
+      // create deserializer
+      CsvSerializer serializer = new CsvSerializer();
+      serializer.setMinimumAnchorConfidence(null);
+      
+      ParameterSet configuration = serializer.configure(new ParameterSet(), g.getSchema());
+      // for (Parameter p : configuration.values()) System.out.println("config " + p.getName() + " = " + p.getValue());
+      assertEquals(0, serializer.configure(configuration, g.getSchema()).size());
+      
+      // no layers required
+      String[] requiredLayers = serializer.getRequiredLayers();
+      assertEquals(0, requiredLayers.length);
+      
+      // serialize
+      Graph[] graphs = { g };
+      File dir = getDir();
+      String[] layerIds = {"who", "word"};
+      NamedStream[] streams = serializer.serialize(graphs, layerIds);
+      assertEquals(1, streams.length);
+      streams[0].save(dir);
+      
+      File actual = new File(dir, streams[0].getName());
+      String differences = diff(
+         new File(dir, "expected_" + streams[0].getName()),
+         actual);
+      if (differences != null)
+      {
+         fail(differences);
+      }
+      else
+      {
+         actual.delete();
+      }
+   }
+
+   @Test public void withAttributes() 
+      throws Exception
+   {
+      Graph g = createGraph("withAttributes.trs");
+      
+      // create deserializer
+      CsvSerializer serializer = new CsvSerializer();
+      
+      ParameterSet configuration = serializer.configure(new ParameterSet(), g.getSchema());
+      // for (Parameter p : configuration.values()) System.out.println("config " + p.getName() + " = " + p.getValue());
+      assertEquals(0, serializer.configure(configuration, g.getSchema()).size());
+      
+      // no layers required
+      String[] requiredLayers = serializer.getRequiredLayers();
+      assertEquals(0, requiredLayers.length);
+      
+      // serialize
+      Graph[] graphs = { g };
+      File dir = getDir();
+      String[] layerIds = {"transcript_transcriber", "who", "word"};
+      NamedStream[] streams = serializer.serialize(graphs, layerIds);
+      assertEquals(1, streams.length);
+      streams[0].save(dir);
+
+      File actual = new File(dir, streams[0].getName());
+      String differences = diff(
+         new File(dir, "expected_" + streams[0].getName()),
+         actual);
+      if (differences != null)
+      {
+         fail(differences);
+      }
+      else
+      {
+         actual.delete();
+      }
+   }
+
+   @Test public void multipleLayers() 
+      throws Exception
+   {
+      Graph g = createGraph("multipleLayers.trs");
+      
+      // create deserializer
+      CsvSerializer serializer = new CsvSerializer();
+      
+      ParameterSet configuration = serializer.configure(new ParameterSet(), g.getSchema());
+      // for (Parameter p : configuration.values()) System.out.println("config " + p.getName() + " = " + p.getValue());
+      assertEquals(0, serializer.configure(configuration, g.getSchema()).size());
+      
+      // no layers required
+      String[] requiredLayers = serializer.getRequiredLayers();
+      assertEquals(0, requiredLayers.length);
+      
+      // serialize
+      Graph[] graphs = { g };
+      File dir = getDir();
+      String[] layerIds = {"who", "word", "orthography"};
+      NamedStream[] streams = serializer.serialize(graphs, layerIds);
+      assertEquals(1, streams.length);
+      streams[0].save(dir);
+
+      File actual = new File(dir, streams[0].getName());
+      String differences = diff(
+         new File(dir, "expected_" + streams[0].getName()),
+         actual);
+      if (differences != null)
+      {
+         fail(differences);
+      }
+      else
+      {
+         actual.delete();
+      }
+   }
+   
+   /**
+    * Creates a test graph.
+    * @return The graph for testing.
+    */
+   public Graph createGraph(String id)
+   {
+      Graph g = new Graph();
+      g.setId(id);
+      
+      Schema schema = new Schema(
+         "who", "turn", "utterance", "word",
+         new Layer("episode", "Episode")
+         .setAlignment(Constants.ALIGNMENT_NONE) 
+         .setPeers(false)
+         .setPeersOverlap(false)
+         .setSaturated(true),
+         new Layer("transcript_transcriber", "Transcriber")
+         .setAlignment(Constants.ALIGNMENT_NONE)
+         .setPeers(false)
+         .setPeersOverlap(false)
+         .setSaturated(true),
+         new Layer("who", "Participants")
+         .setAlignment(Constants.ALIGNMENT_NONE)
+         .setPeers(true)
+         .setPeersOverlap(true)
+         .setSaturated(true),
+         new Layer("turn", "Speaker turns")
+         .setAlignment(Constants.ALIGNMENT_INTERVAL)
+         .setPeers(true)
+         .setPeersOverlap(false)
+         .setSaturated(false)
+         .setParentId("who")
+         .setParentIncludes(true),  
+         new Layer("utterance", "Utterances")
+         .setAlignment(Constants.ALIGNMENT_INTERVAL)
+         .setPeers(true)
+         .setPeersOverlap(false)
+         .setSaturated(true)
+         .setParentId("turn")
+         .setParentIncludes(true), 
+         new Layer("word", "Words")
+         .setAlignment(Constants.ALIGNMENT_INTERVAL)
+         .setPeers(true)
+         .setPeersOverlap(false)
+         .setSaturated(false)
+         .setParentId("turn")
+         .setParentIncludes(true),
+         new Layer("orthography", "Orthography")
+         .setAlignment(Constants.ALIGNMENT_NONE)
+         .setPeers(false)
+         .setPeersOverlap(false)
+         .setSaturated(true)
+         .setParentId("word")
+         .setParentIncludes(true) 
+         );
+      g.setSchema(schema);
+      
+      g.addAnchor(new Anchor("turnStart", 0.0, Constants.CONFIDENCE_MANUAL));
+      g.addAnchor(new Anchor("a1", 0.0, Constants.CONFIDENCE_AUTOMATIC));
+      g.addAnchor(new Anchor("a2", 1.0, Constants.CONFIDENCE_AUTOMATIC));
+      g.addAnchor(new Anchor("a3a", 3.0, Constants.CONFIDENCE_AUTOMATIC));
+      g.addAnchor(new Anchor("utterance", 3.0, Constants.CONFIDENCE_MANUAL));
+      g.addAnchor(new Anchor("a3b", 4.0, Constants.CONFIDENCE_DEFAULT));
+      g.addAnchor(new Anchor("a4", 5.0, Constants.CONFIDENCE_DEFAULT));
+      g.addAnchor(new Anchor("a5", 6.0, Constants.CONFIDENCE_DEFAULT));
+      g.addAnchor(new Anchor("turnEnd", 6.0, Constants.CONFIDENCE_MANUAL));
+      
+      g.addAnnotation(new Annotation("ep1", "episode", "episode", "turnStart", "turnEnd", "episode"));
+      
+      g.addAnnotation(new Annotation("who1", "john smith", "who", "turnStart", "turnEnd", "test.trs"));
+      g.addAnnotation(new Annotation("who2", "jane doe", "who", "turnStart", "turnEnd", "test.trs"));
+      
+      g.addAnnotation(new Annotation("turn1", "john smith", "turn", "turnStart", "turnEnd", "who1"));
+      
+      g.addAnnotation(new Annotation("utterance1", "john smith", "utterance", "turnStart", "utterance", "turn1"));
+      g.addAnnotation(new Annotation("utterance2", "john smith", "utterance", "utterance", "turnEnd", "turn1"));
+      
+      g.addAnnotation(new Annotation("word1", "The", "word", "a1", "a2", "turn1"));
+      g.addAnnotation(new Annotation("orth1", "the", "orthography", "a1", "a2", "word1"));
+      g.addAnnotation(new Annotation("word2", "quick", "word", "a2", "a3a", "turn1"));
+      g.addAnnotation(new Annotation("orth2", "quick", "orthography", "a2", "a3a", "word2"));
+      g.addAnnotation(new Annotation("word3", "BROWN", "word", "a3b", "a4", "turn1"));
+      g.addAnnotation(new Annotation("ord3", "brown", "orthography", "a2", "a4", "word3"));
+      g.addAnnotation(new Annotation("word4", "fox", "word", "a4", "a5", "turn1"));
+      g.addAnnotation(new Annotation("orth3", "fox", "orthography", "a4", "a5", "word4"));
+
+      g.addAnnotation(new Annotation("trans", "Robert Fromont", "transcript_transcriber", g.getId()));
+
+      return g;
+   } // end of createGraph()
+   
+   /**
+    * Diffs two files.
+    * @param expected
+    * @param actual
+    * @return null if the files are the same, and a String describing differences if not.
+    */
+   public String diff(File expected, File actual)
+   {
+      StringBuffer d = new StringBuffer();
+      
+      try
+      {
+	 // compare with what we expected
+	 Vector<String> actualLines = new Vector<String>();
+	 BufferedReader reader = new BufferedReader(new FileReader(actual));
+	 String line = reader.readLine();
+	 while (line != null)
+	 {
+	    actualLines.add(line);
+	    line = reader.readLine();
+	 }
+	 Vector<String> expectedLines = new Vector<String>();
+	 reader = new BufferedReader(new FileReader(expected));
+	 line = reader.readLine();
+	 while (line != null)
+	 {
+	    expectedLines.add(line);
+	    line = reader.readLine();
+	 }
+	 MinimumEditPath<String> comparator = new MinimumEditPath<String>();
+	 List<EditStep<String>> path = comparator.minimumEditPath(expectedLines, actualLines);
+	 for (EditStep<String> step : path)
+	 {
+	    switch (step.getOperation())
+	    {
+	       case CHANGE:
+		  d.append("\n"+expected.getPath()+":"+(step.getFromIndex()+1)+": Expected:\n" 
+			   + step.getFrom() 
+			   + "\n"+actual.getPath()+":"+(step.getToIndex()+1)+": Found:\n" + step.getTo());
+		  break;
+	       case DELETE:
+		  d.append("\n"+expected.getPath()+":"+(step.getFromIndex()+1)+": Deleted:\n" 
+			   + step.getFrom()
+			   + "\n"+actual.getPath()+":"+(step.getToIndex()+1)+": Missing");
+		  break;
+	       case INSERT:
+		  d.append("\n"+expected.getPath()+":"+(step.getFromIndex()+1)+": Missing" 
+			   + "\n"+actual.getPath()+":"+(step.getToIndex()+1)+": Inserted:\n" 
+			   + step.getTo());
+		  break;
+	    }
+	 } // next step
+      }
+      catch(Exception exception)
+      {
+	 d.append("\n" + exception);
+      }
+      if (d.length() > 0) return d.toString();
+      return null;
+   } // end of diff()
+
+   /**
+    * Directory for text files.
+    * @see #getDir()
+    * @see #setDir(File)
+    */
+   protected File fDir;
+   /**
+    * Getter for {@link #fDir}: Directory for text files.
+    * @return Directory for text files.
+    */
+   public File getDir() 
+   { 
+      if (fDir == null)
+      {
+	 try
+	 {
+	    URL urlThisClass = getClass().getResource(getClass().getSimpleName() + ".class");
+	    File fThisClass = new File(urlThisClass.toURI());
+	    fDir = fThisClass.getParentFile();
+	 }
+	 catch(Throwable t)
+	 {
+	    System.out.println("" + t);
+	 }
+      }
+      return fDir; 
+   }
+   /**
+    * Setter for {@link #fDir}: Directory for text files.
+    * @param fNewDir Directory for text files.
+    */
+   public void setDir(File fNewDir) { fDir = fNewDir; }
+
+   public static void main(String args[]) 
+   {
+      org.junit.runner.JUnitCore.main("nzilbb.ag.serialize.csv.test.TestCsvSerializer");
+   }
+}
