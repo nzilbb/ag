12722601aec7a46e9296df8f4cb60dc8e0c3b7ce
Robert Fromont <robert@fromont.net.nz>
2020-01-23 12:14:10 -0300

    Version 20200123.1213 - track graph changes via ChangeTracker.

--- java/nzilbb/ag/Constants.java
+++ java/nzilbb/ag/Constants.java
@@ -1,5 +1,5 @@
 //
-// Copyright 2015-2019 New Zealand Institute of Language, Brain and Behaviour, 
+// Copyright 2015-2020 New Zealand Institute of Language, Brain and Behaviour, 
 // University of Canterbury
 // Written by Robert Fromont - robert.fromont@canterbury.ac.nz
 //
@@ -30,7 +30,7 @@ public class Constants
 {
 
    /** API version */
-   public static final String VERSION = "20200117.1258";
+   public static final String VERSION = "20200123.1213";
    
    /** Standardized possible value for anchor offset or annotation label confidence - no confidence. Value is 0. */
    public static final int CONFIDENCE_NONE = 0;
