9da7eb49d12f60478a279cef6b3e18f2a19fc4e7
Robert Fromont <robert@fromont.net.nz>
2020-07-08 20:19:56 -0300

    Version 20200708.2018 - first Annotator implementation: PorterStemmer.

--- java/nzilbb/ag/Constants.java
+++ java/nzilbb/ag/Constants.java
@@ -30,7 +30,7 @@ public class Constants
 {
 
    /** API version */
-   public static final String VERSION = "20200306.1746";
+   public static final String VERSION = "20200708.2018";
    
    /** Standardized possible value for anchor offset or annotation label confidence - no confidence. Value is 0. */
    public static final int CONFIDENCE_NONE = 0;
