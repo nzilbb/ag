f34310c76b42088edf30395a921114c07e185cfb
Robert Fromont <robert@fromont.net.nz>
2020-08-31 17:04:22 -0300

    Implemen Kirshenbaum/DISC translations.

---
+++ java/nzilbb/encoding/DISC2Kirshenbaum.java
@@ -0,0 +1,101 @@
+//
+// Copyright 2020 New Zealand Institute of Language, Brain and Behaviour, 
+// University of Canterbury
+// Written by Robert Fromont - robert.fromont@canterbury.ac.nz
+//
+//    This file is part of nzilbb.ag.
+//
+//    nzilbb.ag is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    nzilbb.ag is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with nzilbb.ag; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+package nzilbb.encoding;
+
+import java.util.HashMap;
+
+/**
+ * Translates CELEX-DISC-encoded transcriptions like
+ * <tt>J1n_</tt>
+ * to <a href="http://en.wikipedia.org/wiki/Kirshenbaum">Kirshenbaum</a>-encoded 
+ * phonemic transcriptions like
+ * <tt>tSeIndZ</tt>.
+ * @see Kirshenbaum2DISC
+ * @author Robert Fromont robert@fromont.net.nz
+ */
+public class DISC2Kirshenbaum extends PhonemeTranslator {
+
+   private static HashMap<Character,String> map;
+   
+   /**
+    * Default constructor.
+    */
+   public DISC2Kirshenbaum() {
+      sourceEncoding = "DISC";
+      destinationEncoding = "Kirshenbaum";
+      
+      // populate the static map of individual phones, if it's not already initialized...
+      if (map == null) {
+         map = new HashMap<Character,String>();
+         
+         // vowels
+         map.put('{',"&");
+         map.put('i',"i:");
+         map.put('#',"A:");
+         map.put('Q',"A.");
+         map.put('$',"O:");
+         map.put('u',"u:");
+         map.put('3',"V\"");
+         
+         map.put('1',"eI");
+         map.put('2',"aI");
+         map.put('4',"OI");
+         map.put('5',"@U");
+         map.put('6',"aU");
+         map.put('7',"I@");
+         map.put('8',"E@");
+         map.put('9',"U@");
+         map.put('c',"&~");
+         map.put('q',"A~:");
+         map.put('0',"&~:");
+         map.put('~',"A.~:");
+         
+         // consonants
+         map.put('J',"tS");
+         map.put('_',"dZ");
+         map.put('R',"r*");
+         map.put('C',"N-");
+         map.put('F',"m-");
+         map.put('H',"n-");
+         map.put('P',"l-");
+      }
+   } // end of constructor
+   
+   /**
+    * Translates a phonemic transcription from the source encoding to the destination encoding.
+    * @param source Phonemic transcription in the source encoding.
+    * @return Phonemic transcription in the destination encoding.
+    */ 
+   public String apply(String source) {
+      StringBuffer kirshenbaum = new StringBuffer(source.length() * 3);
+      // for each phone
+      for (int c = 0; c < source.length(); c++) {
+         if (map.containsKey(source.charAt(c))) {
+            kirshenbaum.append(map.get(source.charAt(c)));
+         } else { // unknown or unexceptional phones are passed through
+            kirshenbaum.append(source.charAt(c));
+         }
+      } // next phoneme
+      return kirshenbaum.toString();
+   }
+   
+} // end of class DISC2Kirshenbaum
---
+++ java/nzilbb/encoding/test/TestDISC2Kirshenbaum2DISC.java
@@ -0,0 +1,139 @@
+//
+// Copyright 2020 New Zealand Institute of Language, Brain and Behaviour, 
+// University of Canterbury
+// Written by Robert Fromont - robert.fromont@canterbury.ac.nz
+//
+//    This file is part of nzilbb.ag.
+//
+//    nzilbb.ag is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    nzilbb.ag is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with nzilbb.ag; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+package nzilbb.encoding.test;
+	      
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import nzilbb.encoding.DISC2Kirshenbaum;
+import nzilbb.encoding.Kirshenbaum2DISC;
+
+public class TestDISC2Kirshenbaum2DISC {
+   
+   @Test public void codebook() throws Exception {
+      // Kirshenbaum is a little one-to-one to DISC      
+      String[] aCodebook = {
+	 "#", /* <-> */"A:", // BATH        - odd/father
+	 "{", /* <-> */"&", // TRAP        - at/fast
+	 "V", /* <-> */"V", // STRUT       - hut/but
+	 "$", /* <-> */"O:", // THOUGHT     - ought/fall
+	 "6", /* <-> */"aU", // MOUTH       - cow/how
+	 "@", /* <-> */"@", // schwa       - discuss
+	 "2", /* <-> */"aI", // PRICE       - hide/my
+	 "b", /* <-> */"b",
+	 "J", /* <-> */"tS",
+	 "d", /* <-> */"d",
+	 "D", /* <-> */"D",
+	 "E", /* <-> */"E", // DRESS       - Ed/red
+	 "3", /* <-> */"V\"", // NURSE       - hurt/her
+	 "1", /* <-> */"eI", // FACE        - ate/say
+	 "f", /* <-> */"f", 
+	 "g", /* <-> */"g",
+	 "h", /* <-> */"h",
+	 "I", /* <-> */"I", // KIT         - it/big
+	 "i", /* <-> */"i:", // FLEECE      - eat/bee
+	 "_", /* <-> */"dZ",
+	 "k", /* <-> */"k",
+	 "l", /* <-> */"l",
+	 "m", /* <-> */"m",
+	 "n", /* <-> */"n",
+	 "N", /* <-> */"N",
+	 "5", /* <-> */"@U", // GOAT        - oat/show
+	 "4", /* <-> */"OI", // CHOICE      - toy/boy
+	 "p", /* <-> */"p",
+	 "r", /* <-> */"r",
+	 "R", /* <-> */"r*", // possible linking R
+	 "s", /* <-> */"s",
+	 "S", /* <-> */"S",
+	 "t", /* <-> */"t",
+	 "T", /* <-> */"T",
+	 "U", /* <-> */"U", // FOOT        - hood/should
+	 "u", /* <-> */"u:", // GOOSE       - two/you
+	 "v", /* <-> */"v",
+	 "w", /* <-> */"w",
+	 "j", /* <-> */"j",
+	 "z", /* <-> */"z",
+	 "Z", /* <-> */"Z",
+
+	 "Q", /* <-> */"A.", // LOT         - ought/off
+	 "7", /* <-> */"I@", // NEAR
+	 "8", /* <-> */"E@", // SQUARE
+	 "9", /* <-> */"U@", // CURE
+	 "?", /* <-> */"?", // glottal stop
+	 
+	 "F", /* <-> */"m-", // idealism
+	 "H", /* <-> */"n-", // burden
+	 "P", /* <-> */"l-", // dangle
+	 "C", /* <-> */"N-", // bacon
+	 "0", /* <-> */"&~:", // lingerie
+	 "~", /* <-> */"A.~:", // bouillon
+	 "c", /* <-> */"&~", // timbre
+	 "q", /* <-> */"A~:", // detente
+
+	 // unknown phones
+	 "x", /* <-> */"x", // ASCII letter
+	 "X", /* <-> */"X", // ASCII letter
+	 "/", /* <-> */"/", // ASCII non-letter
+	 "ö", /* <-> */"ö" // UNICODE letter
+      };
+
+      DISC2Kirshenbaum disc2kirshenbaum = new DISC2Kirshenbaum();
+      assertEquals("Encoding name", "DISC", disc2kirshenbaum.getSourceEncoding());
+      assertEquals("Encoding name", "Kirshenbaum", disc2kirshenbaum.getDestinationEncoding());
+      Kirshenbaum2DISC kirshenbaum2disc = new Kirshenbaum2DISC();
+      assertEquals("Encoding name", "Kirshenbaum", kirshenbaum2disc.getSourceEncoding());
+      assertEquals("Encoding name", "DISC", kirshenbaum2disc.getDestinationEncoding());
+
+      StringBuffer sDISC = new StringBuffer(aCodebook.length/2);
+      StringBuffer sKirshenbaum = new StringBuffer(3*aCodebook.length/2);
+      // for each pair of elements into the array
+      for (int i = 0; i < aCodebook.length; i += 2)
+      {
+	 // check  mapping
+	 assertEquals("DISC phoneme " + aCodebook[i],
+                      aCodebook[i+1], disc2kirshenbaum.apply(aCodebook[i]));
+	 assertEquals("Kirshenbaum phoneme " + aCodebook[i+1],
+                      aCodebook[i], kirshenbaum2disc.apply(aCodebook[i+1]));
+         
+	 // accumulate the whole set into strings
+	 sDISC.append(aCodebook[i]);
+	 sKirshenbaum.append(aCodebook[i+1]);
+      } // next pair
+      
+      assertEquals("DISC 'word'",
+                   sKirshenbaum.toString(), disc2kirshenbaum.apply(sDISC.toString()));
+      assertEquals("Kirshenbaum 'word'",
+                   sDISC.toString(), kirshenbaum2disc.apply(sKirshenbaum.toString()));
+
+      // check dangerous cases
+      assertEquals("Sweatshops with syllable marking",
+                   "swEt-SQps", kirshenbaum2disc.apply("swEt-SA.ps"));
+      // without syllable marking, it goes wrong
+      assertEquals("Sweatshops without syllable marking - known failure",
+                   "swEJQps", kirshenbaum2disc.apply("swEtSA.ps"));
+   }
+   
+   public static void main(String args[]) {
+      org.junit.runner.JUnitCore.main("nzilbb.encoding.test.TestDISC2Kirshenbaum2DISC");
+   }
+}
