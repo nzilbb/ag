41f42c7fc745c106f76f837ac6eaa18d4ddf8ad7
Robert Fromont <robert@fromont.net.nz>
2021-03-29 17:03:58 -0300

    Add SemanticVersionComparator.

---
+++ ag/src/main/java/nzilbb/util/SemanticVersionComparator.java
@@ -0,0 +1,90 @@
+//
+// Copyright 2021 New Zealand Institute of Language, Brain and Behaviour, 
+// University of Canterbury
+// Written by Robert Fromont - robert.fromont@canterbury.ac.nz
+//
+//    This file is part of nzilbb.ag.
+//
+//    nzilbb.ag is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    nzilbb.ag is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with nzilbb.ag; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+package nzilbb.util;
+
+import java.util.Comparator;
+import java.text.MessageFormat;
+import java.text.ParseException;
+
+/**
+ * Compares semantic version strings.
+ * @author Robert Fromont robert@fromont.net.nz
+ */
+public class SemanticVersionComparator implements Comparator<String> {
+
+   MessageFormat versionParser = new MessageFormat(
+      "{0,number,integer}.{1,number,integer}.{2,number,integer}{3}");
+   /**
+    * Default constructor.
+    */
+   public SemanticVersionComparator() {
+   } // end of constructor
+
+   /**
+    * Compares its two arguments for order. Returns a negative integer, zero, or a
+    * positive integer as the first argument is less than, equal to, or greater than the
+    * second. 
+    */
+   public int compare(String version1, String version2) {
+      
+      // try to interpret the strings as semantic versions
+      Object[] parse1 = null;
+      try {
+         parse1 = versionParser.parse(version1);
+      } catch(ParseException exception) {}
+      Object[] parse2 = null;
+      try {
+         parse2 = versionParser.parse(version2);
+      } catch(ParseException exception) {}
+
+      // if they're both parseable
+      if (parse1 != null && parse2 != null) {
+         // compare their version parts
+         Long major1 = (Long)parse1[0];
+         Long minor1 = (Long)parse1[1];
+         Long patch1 = (Long)parse1[2];
+         String suffix1 = (String)parse1[3];
+         Long major2 = (Long)parse2[0];
+         Long minor2 = (Long)parse2[1];
+         Long patch2 = (Long)parse2[2];
+         String suffix2 = (String)parse2[3];
+         int comparison = major1.compareTo(major2);
+         if (comparison != 0) return comparison;
+         comparison = minor1.compareTo(minor2);
+         if (comparison != 0) return comparison;
+         comparison = patch1.compareTo(patch2);
+         if (comparison != 0) return comparison;
+         // the version with a suffix is less than the version without
+         if (suffix1.length() == 0 && suffix2.length() > 0) return 1;
+         if (suffix2.length() == 0 && suffix1.length() > 0) return -1;         
+         if (suffix2.length() > 0 && suffix1.length() > 0) return suffix1.compareTo(suffix2);
+         // if we got this far, everything is the same
+         return 0;
+      } else if (parse1 != null) { // version1 is semantic, so it is 'greater than' version2
+         return 10;
+      } else if (parse2 != null) { // version2 is semantic, so it is 'greater than' version1
+         return -10;
+      } else { // neither is semantic, so compare then as plain character strings
+         return version1.compareTo(version2);
+      }
+   }
+} // end of class SemanticVersionComparator
---
+++ ag/src/test/java/nzilbb/util/TestSemanticVersionComparator.java
@@ -0,0 +1,67 @@
+//
+// Copyright 2017-2019 New Zealand Institute of Language, Brain and Behaviour, 
+// University of Canterbury
+// Written by Robert Fromont - robert.fromont@canterbury.ac.nz
+//
+//    This file is part of nzilbb.ag.
+//
+//    nzilbb.ag is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    nzilbb.ag is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with nzilbb.ag; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+package nzilbb.util;
+	      
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TestSemanticVersionComparator {
+   
+   @Test public void basicSemanticComparison() throws Exception {
+      SemanticVersionComparator c = new SemanticVersionComparator();
+      assertTrue("equal", c.compare("1.2.3", "1.2.3") == 0);
+      assertTrue("less", c.compare("1.2.3", "3.2.1") < 0);
+      assertTrue("more", c.compare("3.2.1", "1.2.3") > 0);
+      assertTrue("less - non-ascii order", c.compare("2.3.4", "10.2.1") < 0);
+      assertTrue("more - non-ascii order", c.compare("10.2.1", "2.3.4") > 0);
+   }
+   
+   @Test public void semanticWithSuffixes() throws Exception {
+      SemanticVersionComparator c = new SemanticVersionComparator();
+      assertTrue("equal", c.compare("1.2.3-SNAPSHOT", "1.2.3-SNAPSHOT") == 0);
+      assertTrue("less", c.compare("1.2.3-SNAPSHOT", "1.2.3") < 0);
+      assertTrue("more", c.compare("1.2.3", "1.2.3-SNAPSHOT") > 0);
+      assertTrue("less - non-ascii order", c.compare("2.3.4-SNAPSHOT", "10.2.1") < 0);
+      assertTrue("more - non-ascii order", c.compare("10.2.1-SNAPSHOT", "2.3.4") > 0);
+   }
+   
+   @Test public void semanticWithNonSemantic() throws Exception {
+      SemanticVersionComparator c = new SemanticVersionComparator();
+      assertTrue("less", c.compare("20210329.1700", "1.2.3") < 0);
+      assertTrue("more", c.compare("1.2.3", "20210329.1700") > 0);
+   }
+   
+   @Test public void nonSemantic() throws Exception {
+      SemanticVersionComparator c = new SemanticVersionComparator();
+      assertTrue("equal", c.compare("20210329.1700", "20210329.1700") == 0);
+      assertTrue("less", c.compare("20210329.1700", "20210329.1701") < 0);
+      assertTrue("more", c.compare("20210329.1701", "20210329.1700") > 0);
+
+      assertTrue("less - amost semantic", c.compare("10.2", "2.1") < 0);
+      assertTrue("more - amost semantic", c.compare("2.1", "10.2") > 0);
+   }
+   
+   public static void main(String args[]) {
+      org.junit.runner.JUnitCore.main("nzilbb.util.TestSemanticVersionComparator");
+   }
+}
