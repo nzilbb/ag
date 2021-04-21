a9e8e3a96a5db9c825818d790b2a238027abcc7f
Robert Fromont <robert@fromont.net.nz>
2021-04-21 19:10:44 -0300

    slt-to-textgrid: Implement SALT Transcript to Praat TextGrid converter.

---
+++ converter/slttotextgrid/src/main/java/nzilbb/converter/SltToTextGrid.java
@@ -0,0 +1,201 @@
+//
+// Copyright 2021 New Zealand Institute of Language, Brain and Behaviour, 
+// University of Canterbury
+// Written by Robert Fromont - robert.fromont@canterbury.ac.nz
+//
+//    This file is part of nzilbb.ag.
+//
+//    nzilbb.ag is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    nzilbb.ag is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with nzilbb.ag; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+package nzilbb.converter;
+
+import javax.swing.filechooser.FileNameExtensionFilter;
+import nzilbb.ag.Constants;
+import nzilbb.ag.Layer;
+import nzilbb.ag.Schema;
+import nzilbb.ag.serialize.GraphDeserializer;
+import nzilbb.ag.serialize.GraphSerializer;
+import nzilbb.formatter.praat.TextGridSerialization;
+import nzilbb.formatter.salt.SltSerialization;
+import nzilbb.util.ProgramDescription;
+import nzilbb.util.Switch;
+
+/**
+ * Converts SALT .slt transcripts to Praat TextGrid files.
+ * @author Robert Fromont robert@fromont.net.nz
+ */
+@ProgramDescription(value="Converts SALT .slt transcripts to Praat .TextGrid files",arguments="file1.slt file2.slt ...")
+public class SltToTextGrid extends Converter {
+
+  /**
+   * Default constructor.
+   */
+  public SltToTextGrid() {
+    setDefaultWindowTitle("SALT to Praat TextGrid converter");
+    // default to false, as it's what users of this converter most likely expect,
+    // and it means that if the idea is a round-trip conversion, inline annotations are not lost
+    setSwitch("parseInlineConventions", "false");
+
+    info = "Praat doesn't support meta-data like Dob, Doe, Ethnicity, etc."
+      +" so almost all SALT header meta-data is lost when converting to .eaf."
+      +"\nBy default, inline annotations (mazes, codes, bound morphemes, etc.)"
+      +" are not interpreted and appear as-is in the TextGrid."
+      +" If you want them to be parse (i.e. annotations removed to yield more"
+      +" standard orthography), use --parseInlineConventions";
+  } // end of constructor
+  
+  public static void main(String argv[]) {
+    new SltToTextGrid().mainRun(argv);
+  }
+  
+  /** File filter for identifying files of the correct type */
+  protected FileNameExtensionFilter getFileFilter() {
+    return new FileNameExtensionFilter("SALT transcripts", "slt");
+  }
+
+  /**
+   * Gets the deserializer that #convert(File) uses.
+   * @return The deserializer to use.
+   */
+  public GraphDeserializer getDeserializer() {
+    return new SltSerialization();
+  }
+  
+  /**
+   * Gets the serializer that #convert(File) uses.
+   * @return The serializer to use.
+   */
+  public GraphSerializer getSerializer() {
+    return new TextGridSerialization();
+  }
+  
+  /**
+   * Specify the schema to used by  {@link #convert(File)}.
+   * @return The schema.
+   */
+  public Schema getSchema() {
+    Schema schema = super.getSchema();
+    // include SALT layers
+    schema.addLayer(
+      new Layer("transcript_language", "Language").setAlignment(Constants.ALIGNMENT_NONE)
+      .setPeers(false).setPeersOverlap(false).setSaturated(true));
+    schema.addLayer(
+      new Layer("transcript_doe", "Recording date").setAlignment(Constants.ALIGNMENT_NONE)
+      .setPeers(false).setPeersOverlap(false).setSaturated(true));
+    schema.addLayer(
+      new Layer("transcript_ca", "Current Age").setAlignment(Constants.ALIGNMENT_NONE)
+      .setPeers(false).setPeersOverlap(false).setSaturated(true));
+    schema.addLayer(
+      new Layer("transcript_context", "Context").setAlignment(Constants.ALIGNMENT_NONE)
+      .setPeers(false).setPeersOverlap(false).setSaturated(true));
+    schema.addLayer(
+      new Layer("transcript_subgroup", "Subgroup/Story").setAlignment(Constants.ALIGNMENT_NONE)
+      .setPeers(false).setPeersOverlap(false).setSaturated(true));
+    schema.addLayer(
+      new Layer("transcript_collect", "Collection Point").setAlignment(Constants.ALIGNMENT_NONE)
+      .setPeers(false).setPeersOverlap(false).setSaturated(true));
+    schema.addLayer(
+      new Layer("transcript_location", "Location").setAlignment(Constants.ALIGNMENT_NONE)
+      .setPeers(false).setPeersOverlap(false).setSaturated(true));
+    schema.addLayer(
+      new Layer("comment", "Comments").setAlignment(Constants.ALIGNMENT_INTERVAL)
+      .setPeers(true).setPeersOverlap(true).setSaturated(false));
+    schema.addLayer(
+      new Layer("main_participant", "Target Speaker").setAlignment(Constants.ALIGNMENT_NONE)
+      .setPeers(false).setPeersOverlap(false).setSaturated(true)
+      .setParentId(schema.getParticipantLayerId()).setParentIncludes(true));
+    schema.addLayer(
+      new Layer("participant_id", "Participant ID").setAlignment(Constants.ALIGNMENT_NONE)
+      .setPeers(false).setPeersOverlap(false).setSaturated(true)
+      .setParentId(schema.getParticipantLayerId()).setParentIncludes(true));
+    schema.addLayer(
+      new Layer("participant_gender", "Gender").setAlignment(Constants.ALIGNMENT_NONE)
+      .setPeers(false).setPeersOverlap(false).setSaturated(true)
+      .setParentId(schema.getParticipantLayerId()).setParentIncludes(true));
+    schema.addLayer(
+      new Layer("participant_dob", "Birth Date").setAlignment(Constants.ALIGNMENT_NONE)
+      .setPeers(false).setPeersOverlap(false).setSaturated(true)
+      .setParentId(schema.getParticipantLayerId()).setParentIncludes(true));
+    schema.addLayer(
+      new Layer("participant_ethnicity", "Ethnicity").setAlignment(Constants.ALIGNMENT_NONE)
+      .setPeers(false).setPeersOverlap(false).setSaturated(true)
+      .setParentId(schema.getParticipantLayerId()).setParentIncludes(true));
+    schema.addLayer(
+      new Layer("pause", "Pauses").setAlignment(Constants.ALIGNMENT_INTERVAL)
+      .setPeers(true).setPeersOverlap(false).setSaturated(false)
+      .setParentId(schema.getTurnLayerId()).setParentIncludes(true));
+    schema.addLayer(
+      new Layer("cunit", "C-Unit").setAlignment(Constants.ALIGNMENT_INTERVAL)
+      .setPeers(true).setPeersOverlap(false).setSaturated(false)
+      .setParentId(schema.getTurnLayerId()).setParentIncludes(true));
+    schema.addLayer(
+      new Layer("parenthetical", "Parentheticals").setAlignment(Constants.ALIGNMENT_INTERVAL)
+      .setPeers(true).setPeersOverlap(false).setSaturated(false)
+      .setParentId(schema.getTurnLayerId()).setParentIncludes(true));
+    schema.addLayer(
+      new Layer("repetition", "Repetitions").setAlignment(Constants.ALIGNMENT_INTERVAL)
+      .setPeers(true).setPeersOverlap(false).setSaturated(false)
+      .setParentId(schema.getTurnLayerId()).setParentIncludes(true));
+    schema.addLayer(
+      new Layer("error", "Errors").setAlignment(Constants.ALIGNMENT_INTERVAL)
+      .setPeers(true).setPeersOverlap(false).setSaturated(false)
+      .setParentId(schema.getTurnLayerId()).setParentIncludes(true));
+    schema.addLayer(
+      new Layer("code", "Non-error Codes").setAlignment(Constants.ALIGNMENT_INTERVAL)
+      .setPeers(true).setPeersOverlap(false).setSaturated(false)
+      .setParentId(schema.getTurnLayerId()).setParentIncludes(true));
+    schema.addLayer(
+      new Layer("maze", "Mazes").setAlignment(Constants.ALIGNMENT_INTERVAL)
+      .setPeers(true).setPeersOverlap(false).setSaturated(false)
+      .setParentId(schema.getTurnLayerId()).setParentIncludes(true));
+    schema.addLayer(
+      new Layer("noise", "Verbal sound effects etc.").setAlignment(Constants.ALIGNMENT_INTERVAL)
+      .setPeers(false).setPeersOverlap(false).setSaturated(true)
+      .setParentId(schema.getTurnLayerId()).setParentIncludes(true));
+    schema.addLayer(
+      new Layer("entity", "Proper Names").setAlignment(Constants.ALIGNMENT_INTERVAL)
+      .setPeers(false).setPeersOverlap(false).setSaturated(true)
+      .setParentId(schema.getTurnLayerId()).setParentIncludes(true));
+    schema.addLayer(
+      new Layer("omission", "Omissions").setAlignment(Constants.ALIGNMENT_INTERVAL)
+      .setPeers(true).setPeersOverlap(false).setSaturated(false)
+      .setParentId(schema.getTurnLayerId()).setParentIncludes(true));
+    schema.addLayer(
+      new Layer("root", "Root form").setAlignment(Constants.ALIGNMENT_NONE)
+      .setPeers(false).setPeersOverlap(false).setSaturated(true)
+      .setParentId(schema.getWordLayerId()).setParentIncludes(true));
+    schema.addLayer(
+      new Layer("bound_morpheme", "Bound Morphemes").setAlignment(Constants.ALIGNMENT_NONE)
+      .setPeers(false).setPeersOverlap(false).setSaturated(true)
+      .setParentId(schema.getWordLayerId()).setParentIncludes(true));
+    schema.addLayer(
+      new Layer("partial_word", "Partial Words").setAlignment(Constants.ALIGNMENT_NONE)
+      .setPeers(false).setPeersOverlap(false).setSaturated(true)
+      .setParentId(schema.getWordLayerId()).setParentIncludes(true));
+    return schema;
+  } // end of getSchema()
+  
+  /**
+   * Specifies which layers should be given to the serializer. The default implementaion
+   * returns only the "utterance" layer.
+   * @return An array of layer IDs.
+   */
+  public String[] getLayersToSerialize() {
+    String[] layers = { "utterance" };
+    return layers;
+  } // end of getLayersToSerialize()
+  
+  private static final long serialVersionUID = -1;
+} // end of class SltToTextGrid
