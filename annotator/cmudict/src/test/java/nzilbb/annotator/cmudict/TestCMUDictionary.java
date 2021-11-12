8c7ef91e926a6c34a5c995c79c9b68a3f16be35c
Robert Fromont <robert@fromont.net.nz>
2021-11-12 17:01:14 -0300

    Indenting only.

--- annotator/cmudict/src/test/java/nzilbb/annotator/cmudict/TestCMUDictionary.java
+++ annotator/cmudict/src/test/java/nzilbb/annotator/cmudict/TestCMUDictionary.java
@@ -48,192 +48,192 @@ import nzilbb.sql.derby.DerbyConnectionFactory;
 
 public class TestCMUDictionary {
 
-   static CMUDictionaryTagger annotator = new CMUDictionaryTagger();
+  static CMUDictionaryTagger annotator = new CMUDictionaryTagger();
    
-   @BeforeClass
-   public static void install() throws Exception {
+  @BeforeClass
+  public static void install() throws Exception {
 
-      System.out.println("Installing lexicon if necessary...");
+    System.out.println("Installing lexicon if necessary...");
 
-      // find the current directory
-      File dir = dir();
+    // find the current directory
+    File dir = dir();
 
-      // set the schema
-      annotator.setSchema(schema());
+    // set the schema
+    annotator.setSchema(schema());
 
-      // set the working directory
-      annotator.setWorkingDirectory(dir);
+    // set the working directory
+    annotator.setWorkingDirectory(dir);
 
-      // use derby for relational database
-      annotator.setRdbConnectionFactory(new DerbyConnectionFactory(dir));
+    // use derby for relational database
+    annotator.setRdbConnectionFactory(new DerbyConnectionFactory(dir));
 
-      // set the annotator configuration, which will install the lexicon the first time (only)
-      annotator.setConfig(annotator.getConfig());
+    // set the annotator configuration, which will install the lexicon the first time (only)
+    annotator.setConfig(annotator.getConfig());
       
-      System.out.println("Lexicon installed.");
-   }
+    System.out.println("Lexicon installed.");
+  }
 	 
-   public static File dir() throws Exception { 
-      URL urlThisClass = TestCMUDictionary.class.getResource(
-         TestCMUDictionary.class.getSimpleName() + ".class");
-      File fThisClass = new File(urlThisClass.toURI());
-      return fThisClass.getParentFile();
-   }
+  public static File dir() throws Exception { 
+    URL urlThisClass = TestCMUDictionary.class.getResource(
+      TestCMUDictionary.class.getSimpleName() + ".class");
+    File fThisClass = new File(urlThisClass.toURI());
+    return fThisClass.getParentFile();
+  }
    
-   @Test public void basicInfo() throws Exception {
+  @Test public void basicInfo() throws Exception {
       
-      CMUDictionary dict = (CMUDictionary)annotator.getDictionary("cmudict");
-      List<String> entries = dict.lookup("TEST");
-      assertFalse("Isn't read only",
-                  dict.isReadOnly());
-      assertEquals("125K odd entries",
-                   125074, dict.countAllKeys());
-      assertEquals("No editable keys",
-                   0, dict.countEditableKeys());
-   }   
+    CMUDictionary dict = (CMUDictionary)annotator.getDictionary("cmudict");
+    List<String> entries = dict.lookup("TEST");
+    assertFalse("Isn't read only",
+                dict.isReadOnly());
+    assertEquals("125K odd entries",
+                 125074, dict.countAllKeys());
+    assertEquals("No editable keys",
+                 0, dict.countEditableKeys());
+  }   
    
-   @Test public void basicLookup() throws Exception {
+  @Test public void basicLookup() throws Exception {
       
-      CMUDictionary dict = (CMUDictionary)annotator.getDictionary("cmudict");
-      List<String> entries = dict.lookup("TEST");
-      assertEquals("1 entry: " + entries, 1, entries.size());
-      assertEquals("UPPERCASE",
-                   "T EH1 S T", entries.iterator().next());
-      assertEquals("lowercase",
-                   "T EH1 S T", dict.lookup("test").iterator().next());
-      assertEquals("more than one entry",
-                   4, dict.lookup("Buenos-Aires").size());
-      assertEquals("no entries",
-                   0, dict.lookup("Fizzgig").size());
-   }   
+    CMUDictionary dict = (CMUDictionary)annotator.getDictionary("cmudict");
+    List<String> entries = dict.lookup("TEST");
+    assertEquals("1 entry: " + entries, 1, entries.size());
+    assertEquals("UPPERCASE",
+                 "T EH1 S T", entries.iterator().next());
+    assertEquals("lowercase",
+                 "T EH1 S T", dict.lookup("test").iterator().next());
+    assertEquals("more than one entry",
+                 4, dict.lookup("Buenos-Aires").size());
+    assertEquals("no entries",
+                 0, dict.lookup("Fizzgig").size());
+  }   
    
-   @Test public void DISC() throws Exception {
+  @Test public void DISC() throws Exception {
       
-      CMUDictionary dict = ((CMUDictionary)annotator.getDictionary("cmudict"))
-         .setEncoding(CMUDictionary.Encoding.DISC);
-      List<String> entries = dict.lookup("transcription");
-      assertEquals("DSIC",
-                   "tr{nskrIpSVn", entries.iterator().next());
-   }   
+    CMUDictionary dict = ((CMUDictionary)annotator.getDictionary("cmudict"))
+      .setEncoding(CMUDictionary.Encoding.DISC);
+    List<String> entries = dict.lookup("transcription");
+    assertEquals("DSIC",
+                 "tr{nskrIpSVn", entries.iterator().next());
+  }   
    
-   @Test public void pagination() throws Exception {
+  @Test public void pagination() throws Exception {
       
-      CMUDictionary dict = (CMUDictionary)annotator.getDictionary("cmudict");
-      Map<String, List<String>> page1 = dict.listAllEntries(0, 10);
-      assertEquals("page 1 size",
-                   10, page1.size());
-      Map<String, List<String>> page2 = dict.listAllEntries(1, 10);
-      assertEquals("page 2 size",
-                   10, page2.size());
-      assertNotEquals("pages are different",
-                      page2.keySet(), page1.keySet());
-   }   
+    CMUDictionary dict = (CMUDictionary)annotator.getDictionary("cmudict");
+    Map<String, List<String>> page1 = dict.listAllEntries(0, 10);
+    assertEquals("page 1 size",
+                 10, page1.size());
+    Map<String, List<String>> page2 = dict.listAllEntries(1, 10);
+    assertEquals("page 2 size",
+                 10, page2.size());
+    assertNotEquals("pages are different",
+                    page2.keySet(), page1.keySet());
+  }   
 
-   @Test public void aggregation() throws Exception {
+  @Test public void aggregation() throws Exception {
       
-      CMUDictionary dict = (CMUDictionary)annotator.getDictionary("cmudict");
-      assertEquals("COUNT keys",
-                   ""+dict.countAllKeys(), dict.aggregateKeys("COUNT"));
-      assertEquals("MIN key",
-                   "!exclamation-point", dict.aggregateKeys("MIN"));
-      assertEquals("MAX key",
-                   "}right-brace", dict.aggregateKeys("MAX"));
-      assertEquals("COUNT entries",
-                   "133854", dict.aggregateEntries("COUNT"));
-      assertEquals("MIN entry",
-                   "AA0 B AA0 T IY0 EH1 L OW0", dict.aggregateEntries("MIN"));
-      assertEquals("MAX entry",
-                   "ZH W EY1 D AO1 NG", dict.aggregateEntries("MAX"));
-   }   
+    CMUDictionary dict = (CMUDictionary)annotator.getDictionary("cmudict");
+    assertEquals("COUNT keys",
+                 ""+dict.countAllKeys(), dict.aggregateKeys("COUNT"));
+    assertEquals("MIN key",
+                 "!exclamation-point", dict.aggregateKeys("MIN"));
+    assertEquals("MAX key",
+                 "}right-brace", dict.aggregateKeys("MAX"));
+    assertEquals("COUNT entries",
+                 "133854", dict.aggregateEntries("COUNT"));
+    assertEquals("MIN entry",
+                 "AA0 B AA0 T IY0 EH1 L OW0", dict.aggregateEntries("MIN"));
+    assertEquals("MAX entry",
+                 "ZH W EY1 D AO1 NG", dict.aggregateEntries("MAX"));
+  }   
 
-   @Test public void readWrite() throws Exception {
+  @Test public void readWrite() throws Exception {
       
-      CMUDictionary dict = (CMUDictionary)annotator.getDictionary("cmudict");
-      assertEquals("lookup",
-                   1, dict.lookup("test").size());
-      assertEquals("lookupEditableEntry",
-                   0, dict.lookupEditableEntry("test").size());
+    CMUDictionary dict = (CMUDictionary)annotator.getDictionary("cmudict");
+    assertEquals("lookup",
+                 1, dict.lookup("test").size());
+    assertEquals("lookupEditableEntry",
+                 0, dict.lookupEditableEntry("test").size());
 
-      try {
-         // add an entry
-         dict.add("floof","F L UW1 F");
-         dict.add("floof","F L UH F"); // not UH1 because otherwise DISC->CMU doesn't match
-         dict.add("floof","F L AW1 F");
-         List<String> entries = dict.lookup("floof");
-         assertEquals("3 entries: " + entries,
-                      3, entries.size());
-         Iterator<String> pron = entries.iterator();
-         assertEquals("F L UW1 F", pron.next());
-         assertEquals("F L UH F", pron.next());
-         assertEquals("F L AW1 F", pron.next());
-         assertEquals("one editable key",
-                      1, dict.countEditableKeys());
-         assertEquals("one editable entry",
-                      1, dict.listEditableEntries().size());
-         List<String> editableEntries = dict.lookupEditableEntry("floof");
-         assertEquals("lookup and lookupEditableEntry return the same thing",
-                      entries, editableEntries);
+    try {
+      // add an entry
+      dict.add("floof","F L UW1 F");
+      dict.add("floof","F L UH F"); // not UH1 because otherwise DISC->CMU doesn't match
+      dict.add("floof","F L AW1 F");
+      List<String> entries = dict.lookup("floof");
+      assertEquals("3 entries: " + entries,
+                   3, entries.size());
+      Iterator<String> pron = entries.iterator();
+      assertEquals("F L UW1 F", pron.next());
+      assertEquals("F L UH F", pron.next());
+      assertEquals("F L AW1 F", pron.next());
+      assertEquals("one editable key",
+                   1, dict.countEditableKeys());
+      assertEquals("one editable entry",
+                   1, dict.listEditableEntries().size());
+      List<String> editableEntries = dict.lookupEditableEntry("floof");
+      assertEquals("lookup and lookupEditableEntry return the same thing",
+                   entries, editableEntries);
 
-         // DISC encoding
-         dict.setEncoding(CMUDictionary.Encoding.DISC);
-         entries = dict.lookup("floof");
-         pron = entries.iterator();
-         assertEquals("fluf", pron.next());
-         assertEquals("flUf", pron.next());
-         assertEquals("fl6f", pron.next());
+      // DISC encoding
+      dict.setEncoding(CMUDictionary.Encoding.DISC);
+      entries = dict.lookup("floof");
+      pron = entries.iterator();
+      assertEquals("fluf", pron.next());
+      assertEquals("flUf", pron.next());
+      assertEquals("fl6f", pron.next());
 
-         dict.remove("floof", "flUf");
-         entries = dict.lookup("floof");
-         assertEquals("remove specific DISC-encoded entry - now 2 entries: " + entries,
-                      2, entries.size());
+      dict.remove("floof", "flUf");
+      entries = dict.lookup("floof");
+      assertEquals("remove specific DISC-encoded entry - now 2 entries: " + entries,
+                   2, entries.size());
 
-         // back to CMU encoding
-         dict.setEncoding(CMUDictionary.Encoding.CMU);
+      // back to CMU encoding
+      dict.setEncoding(CMUDictionary.Encoding.CMU);
                      
-         entries = dict.lookup("floof");
-         pron = entries.iterator();
-         assertEquals("remove specific CMU-encoded entry",
-                      "F L UW1 F", pron.next());
-         assertEquals("remove specific CMU-encoded entry",
-                      "F L AW1 F", pron.next());
+      entries = dict.lookup("floof");
+      pron = entries.iterator();
+      assertEquals("remove specific CMU-encoded entry",
+                   "F L UW1 F", pron.next());
+      assertEquals("remove specific CMU-encoded entry",
+                   "F L AW1 F", pron.next());
 
-         dict.remove("floof");
-         entries = dict.lookup("floof");
-         assertEquals("remove all entries - now no entries: " + entries,
-                      0, entries.size());
-      } finally {
-         try { dict.remove("floof"); } catch(Exception exception) {}
-      }
-   }   
+      dict.remove("floof");
+      entries = dict.lookup("floof");
+      assertEquals("remove all entries - now no entries: " + entries,
+                   0, entries.size());
+    } finally {
+      try { dict.remove("floof"); } catch(Exception exception) {}
+    }
+  }   
    
-   /**
-    * Returns a graph for annotating.
-    * @return The graph for testing with.
-    */
-   public static Schema schema() {
-      return new Schema(
-         "who", "turn", "utterance", "word",
-         new Layer("transcript_language", "Overall Language")
-         .setAlignment(Constants.ALIGNMENT_NONE)
-         .setPeers(false).setPeersOverlap(false).setSaturated(true),
-         new Layer("participant", "Participants").setAlignment(Constants.ALIGNMENT_NONE)
-         .setPeers(true).setPeersOverlap(true).setSaturated(true),
-         new Layer("turn", "Speaker turns").setAlignment(Constants.ALIGNMENT_INTERVAL)
-         .setPeers(true).setPeersOverlap(false).setSaturated(false)
-         .setParentId("participant").setParentIncludes(true),
-         new Layer("utterance", "Utterances").setAlignment(Constants.ALIGNMENT_INTERVAL)
-         .setPeers(true).setPeersOverlap(false).setSaturated(true)
-         .setParentId("turn").setParentIncludes(true),
-         new Layer("lang", "Phrase Language").setAlignment(Constants.ALIGNMENT_INTERVAL)
-         .setPeers(true).setPeersOverlap(false).setSaturated(false)
-         .setParentId("turn").setParentIncludes(true),
-         new Layer("word", "Words").setAlignment(Constants.ALIGNMENT_INTERVAL)
-         .setPeers(true).setPeersOverlap(false).setSaturated(false)
-         .setParentId("turn").setParentIncludes(true));
-   } // end of schema()
+  /**
+   * Returns a graph for annotating.
+   * @return The graph for testing with.
+   */
+  public static Schema schema() {
+    return new Schema(
+      "who", "turn", "utterance", "word",
+      new Layer("transcript_language", "Overall Language")
+      .setAlignment(Constants.ALIGNMENT_NONE)
+      .setPeers(false).setPeersOverlap(false).setSaturated(true),
+      new Layer("participant", "Participants").setAlignment(Constants.ALIGNMENT_NONE)
+      .setPeers(true).setPeersOverlap(true).setSaturated(true),
+      new Layer("turn", "Speaker turns").setAlignment(Constants.ALIGNMENT_INTERVAL)
+      .setPeers(true).setPeersOverlap(false).setSaturated(false)
+      .setParentId("participant").setParentIncludes(true),
+      new Layer("utterance", "Utterances").setAlignment(Constants.ALIGNMENT_INTERVAL)
+      .setPeers(true).setPeersOverlap(false).setSaturated(true)
+      .setParentId("turn").setParentIncludes(true),
+      new Layer("lang", "Phrase Language").setAlignment(Constants.ALIGNMENT_INTERVAL)
+      .setPeers(true).setPeersOverlap(false).setSaturated(false)
+      .setParentId("turn").setParentIncludes(true),
+      new Layer("word", "Words").setAlignment(Constants.ALIGNMENT_INTERVAL)
+      .setPeers(true).setPeersOverlap(false).setSaturated(false)
+      .setParentId("turn").setParentIncludes(true));
+  } // end of schema()
    
 
-   public static void main(String args[]) {
-      org.junit.runner.JUnitCore.main("nzilbb.annotator.cmudict.TestCMUDictionary");
-   }
+  public static void main(String args[]) {
+    org.junit.runner.JUnitCore.main("nzilbb.annotator.cmudict.TestCMUDictionary");
+  }
 }
