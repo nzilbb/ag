4db3db9080aa5e38f1debf77c3e920eb6746dc43
Robert Fromont <robert@fromont.net.nz>
2021-12-14 11:32:08 -0300

    Add nzilbb.sql.derby

---
+++ ag/src/main/java/nzilbb/sql/derby/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Implementation for providing connections to 
+ * <a href="https://db.apache.org/derby/">Derby</a> databases.
+ * <p> Also provides a translation from MySQL-oriented SQL to the Derby SQL dialect.
+ * @author Robert Fromont robert.fromont@canterbury.ac.nz
+ */
+package nzilbb.sql.derby;
