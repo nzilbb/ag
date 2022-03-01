fb79e47bc1c7f1776eade2fe4c30ddaad28ed5c6
Robert Fromont <robert@fromont.net.nz>
2022-03-01 19:09:42 -0300

    Indenting only.

--- ag/src/main/java/nzilbb/ag/automation/util/AnnotatorDescriptor.java
+++ ag/src/main/java/nzilbb/ag/automation/util/AnnotatorDescriptor.java
@@ -43,224 +43,224 @@ import nzilbb.util.IO;
  */
 public class AnnotatorDescriptor implements CloneableBean {
    
-   // Attributes:
+  // Attributes:
 
-   /**
-    * Fully-qualified annotator class name.
-    * @see #getAnnotatorClassName()
-    */
-   protected String annotatorClassName;
-   /**
-    * Getter for {@link #annotatorClassName}: Fully-qualified annotator class name.
-    * @return Fully-qualified annotator class name.
-    */
-   public String getAnnotatorClassName() { return annotatorClassName; }
+  /**
+   * Fully-qualified annotator class name.
+   * @see #getAnnotatorClassName()
+   */
+  protected String annotatorClassName;
+  /**
+   * Getter for {@link #annotatorClassName}: Fully-qualified annotator class name.
+   * @return Fully-qualified annotator class name.
+   */
+  public String getAnnotatorClassName() { return annotatorClassName; }
 
-   /**
-    * Source of the class implementation.
-    * @see #getAnnotatorClassLoader()
-    */
-   protected ClassLoader annotatorClassLoader;
-   /**
-    * Getter for {@link #annotatorClassLoader}: Source of the class implementation.
-    * @return Source of the class implementation.
-    */
-   public ClassLoader getAnnotatorClassLoader() { return annotatorClassLoader; }
+  /**
+   * Source of the class implementation.
+   * @see #getAnnotatorClassLoader()
+   */
+  protected ClassLoader annotatorClassLoader;
+  /**
+   * Getter for {@link #annotatorClassLoader}: Source of the class implementation.
+   * @return Source of the class implementation.
+   */
+  public ClassLoader getAnnotatorClassLoader() { return annotatorClassLoader; }
 
-   /**
-    * Implementing class of the annotator.
-    * @see #getAnnotatorClass()
-    */
-   @SuppressWarnings("rawtypes")
-   protected Class annotatorClass;
-   /**
-    * Getter for {@link #annotatorClass}: Implementing class of the annotator.
-    * @return Implementing class of the annotator.
-    */
-   @SuppressWarnings("rawtypes")
-   public Class getAnnotatorClass() { return annotatorClass; }
+  /**
+   * Implementing class of the annotator.
+   * @see #getAnnotatorClass()
+   */
+  @SuppressWarnings("rawtypes")
+  protected Class annotatorClass;
+  /**
+   * Getter for {@link #annotatorClass}: Implementing class of the annotator.
+   * @return Implementing class of the annotator.
+   */
+  @SuppressWarnings("rawtypes")
+  public Class getAnnotatorClass() { return annotatorClass; }
 
-   /**
-    * Instance of the annotator class.
-    * @see #getInstance()
-    */
-   protected Annotator instance;
-   /**
-    * Getter for {@link #instance}: Instance of the annotator class.
-    * @return Instance of the annotator class.
-    */
-   public Annotator getInstance() { return instance; }
+  /**
+   * Instance of the annotator class.
+   * @see #getInstance()
+   */
+  protected Annotator instance;
+  /**
+   * Getter for {@link #instance}: Instance of the annotator class.
+   * @return Instance of the annotator class.
+   */
+  public Annotator getInstance() { return instance; }
    
-   /**
-    * Unique name for the annotator, which is immutable across versions of the implemetantation.
-    * @return The annotator's ID.
-    */
-   @ClonedProperty
-   public String getAnnotatorId() {
-      return instance.getAnnotatorId();
-   }
+  /**
+   * Unique name for the annotator, which is immutable across versions of the implemetantation.
+   * @return The annotator's ID.
+   */
+  @ClonedProperty
+  public String getAnnotatorId() {
+    return instance.getAnnotatorId();
+  }
 
-   /**
-    * Version of this implementation; versions will typically be numeric, but this is not
-    * a requirement.
-    * @return Annotator version.
-    */
-   @ClonedProperty
-   public String getVersion() {
-      return instance.getVersion();
-   }
-   /**
-    * Get the minimum version of the nzilbb.ag API supported by the serializer. 
-    * @return Minimum version of the nzilbb.ag API supported by the serializer.
-    * @see nzilbb.ag.Constants#VERSION
-    */
-   @ClonedProperty
-   public String getMinimumApiVersion() {
-      return instance.getMinimumApiVersion();
-   }
+  /**
+   * Version of this implementation; versions will typically be numeric, but this is not
+   * a requirement.
+   * @return Annotator version.
+   */
+  @ClonedProperty
+  public String getVersion() {
+    return instance.getVersion();
+  }
+  /**
+   * Get the minimum version of the nzilbb.ag API supported by the serializer. 
+   * @return Minimum version of the nzilbb.ag API supported by the serializer.
+   * @see nzilbb.ag.Constants#VERSION
+   */
+  @ClonedProperty
+  public String getMinimumApiVersion() {
+    return instance.getMinimumApiVersion();
+  }
    
-   // Methods:
+  // Methods:
    
-   /**
-    * Constructor.
-    * @param annotatorClassName Fully-qualified annotator class name.
-    * @param annotatorClassLoader Source of the class implementation.
-    * @throws ClassNotFoundException If the annotator is not found by 
-    * <var>annotatorClassLoader</var>.
-    * @throws NoSuchMethodException If the annotator has no default constructor.
-    * @throws IllegalAccessException If the annotator's default constructor is not public.
-    * @throws InvocationTargetException If the annotator's constructor throws an exception.
-    * @throws InstantiationException If the annotator is an abstract class.
-    * @throws ClassCastException If the annotator does not extend {@link Annotator}.
-    */
-   @SuppressWarnings("unchecked")
-   public AnnotatorDescriptor(String annotatorClassName, ClassLoader annotatorClassLoader)
-      throws ClassNotFoundException, NoSuchMethodException, InstantiationException,
-      IllegalAccessException, InvocationTargetException, ClassCastException {
-      this.annotatorClassName = annotatorClassName;
-      this.annotatorClassLoader = annotatorClassLoader;
-      this.annotatorClass = Class.forName(annotatorClassName, true, annotatorClassLoader);
-      this.instance = (Annotator)this.annotatorClass.getConstructor().newInstance();
-   } // end of constructor
+  /**
+   * Constructor.
+   * @param annotatorClassName Fully-qualified annotator class name.
+   * @param annotatorClassLoader Source of the class implementation.
+   * @throws ClassNotFoundException If the annotator is not found by 
+   * <var>annotatorClassLoader</var>.
+   * @throws NoSuchMethodException If the annotator has no default constructor.
+   * @throws IllegalAccessException If the annotator's default constructor is not public.
+   * @throws InvocationTargetException If the annotator's constructor throws an exception.
+   * @throws InstantiationException If the annotator is an abstract class.
+   * @throws ClassCastException If the annotator does not extend {@link Annotator}.
+   */
+  @SuppressWarnings("unchecked")
+  public AnnotatorDescriptor(String annotatorClassName, ClassLoader annotatorClassLoader)
+    throws ClassNotFoundException, NoSuchMethodException, InstantiationException,
+    IllegalAccessException, InvocationTargetException, ClassCastException {
+    this.annotatorClassName = annotatorClassName;
+    this.annotatorClassLoader = annotatorClassLoader;
+    this.annotatorClass = Class.forName(annotatorClassName, true, annotatorClassLoader);
+    this.instance = (Annotator)this.annotatorClass.getConstructor().newInstance();
+  } // end of constructor
 
-   /**
-    * Constructor.
-    * @param annotatorJar Java archive that contains the annotator implementation.
-    * @throws ClassNotFoundException If no annotator is found.
-    * @throws NoSuchMethodException If the annotator has no default constructor.
-    * @throws IllegalAccessException If the annotator's default constructor is not public.
-    * @throws InvocationTargetException If the annotator's constructor throws an exception.
-    * @throws InstantiationException If the annotator is an abstract class.
-    * @throws ClassCastException If the annotator does not extend {@link Annotator}.
-    */
-   @SuppressWarnings("unchecked")
-   public AnnotatorDescriptor(File annotatorJar) throws ClassNotFoundException, IOException {
+  /**
+   * Constructor.
+   * @param annotatorJar Java archive that contains the annotator implementation.
+   * @throws ClassNotFoundException If no annotator is found.
+   * @throws NoSuchMethodException If the annotator has no default constructor.
+   * @throws IllegalAccessException If the annotator's default constructor is not public.
+   * @throws InvocationTargetException If the annotator's constructor throws an exception.
+   * @throws InstantiationException If the annotator is an abstract class.
+   * @throws ClassCastException If the annotator does not extend {@link Annotator}.
+   */
+  @SuppressWarnings("unchecked")
+  public AnnotatorDescriptor(File annotatorJar) throws ClassNotFoundException, IOException {
 
-      if (!annotatorJar.exists()) {
-         throw new FileNotFoundException(annotatorJar.getPath());
-      }
+    if (!annotatorJar.exists()) {
+      throw new FileNotFoundException(annotatorJar.getPath());
+    }
       
-      instance = (Annotator)IO.FindImplementorInJar(
-         annotatorJar, getClass().getClassLoader(), Annotator.class);
-      if (instance == null) throw new ClassNotFoundException(Annotator.class.getName());
+    instance = (Annotator)IO.FindImplementorInJar(
+      annotatorJar, getClass().getClassLoader(), Annotator.class);
+    if (instance == null) throw new ClassNotFoundException(Annotator.class.getName());
 
-      annotatorClass = instance.getClass();
+    annotatorClass = instance.getClass();
 
-      annotatorClassName = annotatorClass.getName();
+    annotatorClassName = annotatorClass.getName();
 
-      annotatorClassLoader = annotatorClass.getClassLoader();
+    annotatorClassLoader = annotatorClass.getClassLoader();
       
-   } // end of constructor
+  } // end of constructor
 
-   /**
-    * Retrieves HTML-encoded description of the function of the annotator, for displaying
-    * to the user.
-    * @return The contents of the info.html file deployed with the annotator, or null if
-    * no information was provided. 
-    */
-   @ClonedProperty
-   public String getInfo() {
-      URL url = annotatorClass.getResource("info.html");
-      if (url == null) return null;
-      try {
-         return IO.InputStreamToString(url.openConnection().getInputStream());
-      } catch(IOException exception) {
-         return null;
-      }
-   } // end of getInfo()
+  /**
+   * Retrieves HTML-encoded description of the function of the annotator, for displaying
+   * to the user.
+   * @return The contents of the info.html file deployed with the annotator, or null if
+   * no information was provided. 
+   */
+  @ClonedProperty
+  public String getInfo() {
+    URL url = annotatorClass.getResource("info.html");
+    if (url == null) return null;
+    try {
+      return IO.InputStreamToString(url.openConnection().getInputStream());
+    } catch(IOException exception) {
+      return null;
+    }
+  } // end of getInfo()
    
-   /**
-    * Determines whether the annotator includes a web-app for installation or general
-    * configuration.
-    * @return true if the class includes a web-app at config/index.html, false otherwise.
-    */
-   public boolean hasConfigWebapp() {
-      return annotatorClass.getResource("config/index.html") != null;
-   } // end of hasConfigWebapp()
+  /**
+   * Determines whether the annotator includes a web-app for installation or general
+   * configuration.
+   * @return true if the class includes a web-app at config/index.html, false otherwise.
+   */
+  public boolean hasConfigWebapp() {
+    return annotatorClass.getResource("config/index.html") != null;
+  } // end of hasConfigWebapp()
 
-   /**
-    * Bean-getter version of {@link #hasConfigWebapp}
-    */
-   @ClonedProperty
-   public Boolean getHasConfigWebapp() {
-      return hasConfigWebapp();
-   }
+  /**
+   * Bean-getter version of {@link #hasConfigWebapp}
+   */
+  @ClonedProperty
+  public Boolean getHasConfigWebapp() {
+    return hasConfigWebapp();
+  }
 
-   /**
-    * Determines whether the annotator includes a web-app for task parameter configuration.
-    * @return true if the class includes a web-app at task/index.html, false otherwise.
-    */
-   public boolean hasTaskWebapp() {
-      return annotatorClass.getResource("task/index.html") != null;
-   } // end of hasTaskWebapp()
+  /**
+   * Determines whether the annotator includes a web-app for task parameter configuration.
+   * @return true if the class includes a web-app at task/index.html, false otherwise.
+   */
+  public boolean hasTaskWebapp() {
+    return annotatorClass.getResource("task/index.html") != null;
+  } // end of hasTaskWebapp()
 
-   /**
-    * Bean-getter version of {@link #hasTaskWebapp}
-    */
-   @ClonedProperty
-   public Boolean getHasTaskWebapp() {
-      return hasTaskWebapp();
-   }
+  /**
+   * Bean-getter version of {@link #hasTaskWebapp}
+   */
+  @ClonedProperty
+  public Boolean getHasTaskWebapp() {
+    return hasTaskWebapp();
+  }
 
-   /**
-    * Determines whether the annotator includes an extras web-app.
-    * @return true if the class includes a web-app at ext/index.html, false otherwise.
-    */
-   public boolean hasExtWebapp() {
-      return annotatorClass.getResource("ext/index.html") != null;
-   } // end of hasTaskWebapp()
+  /**
+   * Determines whether the annotator includes an extras web-app.
+   * @return true if the class includes a web-app at ext/index.html, false otherwise.
+   */
+  public boolean hasExtWebapp() {
+    return annotatorClass.getResource("ext/index.html") != null;
+  } // end of hasTaskWebapp()
 
-   /**
-    * Bean-getter version of {@link #hasExtWebapp}
-    */
-   @ClonedProperty
-   public Boolean getHasExtWebapp() {
-      return hasExtWebapp();
-   }
+  /**
+   * Bean-getter version of {@link #hasExtWebapp}
+   */
+  @ClonedProperty
+  public Boolean getHasExtWebapp() {
+    return hasExtWebapp();
+  }
 
-   /**
-    * Provides access to the given resource, supplied by the annotator class.
-    * @param path The path to the resource, relative to the annotator class.
-    * @return The content of the resource.
-    */
-   public InputStream getResource(String path) {
-      URL url = annotatorClass.getResource(path);
-      if (url == null) return null;
-      try {
-         return url.openConnection().getInputStream();
-      } catch(IOException exception) {
-         System.out.println("exception " + exception);
-         return null;
-      }
-   } // end of getResource()
+  /**
+   * Provides access to the given resource, supplied by the annotator class.
+   * @param path The path to the resource, relative to the annotator class.
+   * @return The content of the resource.
+   */
+  public InputStream getResource(String path) {
+    URL url = annotatorClass.getResource(path);
+    if (url == null) return null;
+    try {
+      return url.openConnection().getInputStream();
+    } catch(IOException exception) {
+      System.out.println("exception " + exception);
+      return null;
+    }
+  } // end of getResource()
    
-   /**
-    * A description of the annotator.
-    * @return A description of the annotator.
-    */
-   @Override
-   public String toString() {
-      return instance.getAnnotatorId() + " ("+instance.getVersion()+")";
-   } // end of toString()
+  /**
+   * A description of the annotator.
+   * @return A description of the annotator.
+   */
+  @Override
+  public String toString() {
+    return instance.getAnnotatorId() + " ("+instance.getVersion()+")";
+  } // end of toString()
 
 } // end of class AnnotatorDescriptor
