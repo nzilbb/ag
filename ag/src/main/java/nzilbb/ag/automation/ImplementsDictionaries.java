f1cdc5e3a4c7d9abede12ab8f5171ae18077f182
Robert Fromont <robert@fromont.net.nz>
2022-03-30 14:54:25 -0300

    Add ImplementsDictionaries.tagAllInstances definition, to facilitate dictionary filling for HTK etc.

--- ag/src/main/java/nzilbb/ag/automation/ImplementsDictionaries.java
+++ ag/src/main/java/nzilbb/ag/automation/ImplementsDictionaries.java
@@ -1,5 +1,5 @@
 //
-// Copyright 2020 New Zealand Institute of Language, Brain and Behaviour, 
+// Copyright 2020-2022 New Zealand Institute of Language, Brain and Behaviour, 
 // University of Canterbury
 // Written by Robert Fromont - robert.fromont@canterbury.ac.nz
 //
@@ -22,6 +22,10 @@
 package nzilbb.ag.automation;
 
 import java.util.List;
+import nzilbb.ag.GraphStore;
+import nzilbb.ag.GraphTransformer;
+import nzilbb.ag.StoreException;
+import nzilbb.ag.TransformationException;
 
 /**
  * Extend this interface if your {@link Annotator} publishes {@link Dictionary} implementations.
@@ -29,37 +33,54 @@ import java.util.List;
  */
 public interface ImplementsDictionaries {
    
-   /**
-    * Lists the dictionaries implemented by this Annotator.
-    * <p> This method can assume that the following methods have been previously called:
-    * <ul>
-    *  <li> {@link Annotator#setSchema(Schema)} </li>
-    *  <li> {@link Annotator#setTaskParameters(String)} </li>
-    *  <li> {@link Annotator#setWorkingDirectory(File)} (if applicable) </li>
-    *  <li> {@link Annotator#setRdbConnectionFactory(ConnectionFactory)}
-    *       (if applicable) </li>
-    * </ul>
-    * @return A (possibly empty) list of IDs of dictionaries.
-    */
-   public List<String> getDictionaryIds();
+  /**
+   * Lists the dictionaries implemented by this Annotator.
+   * <p> This method can assume that the following methods have been previously called:
+   * <ul>
+   *  <li> {@link Annotator#setSchema(Schema)} </li>
+   *  <li> {@link Annotator#setTaskParameters(String)} </li>
+   *  <li> {@link Annotator#setWorkingDirectory(File)} (if applicable) </li>
+   *  <li> {@link Annotator#setRdbConnectionFactory(ConnectionFactory)}
+   *       (if applicable) </li>
+   * </ul>
+   * @return A (possibly empty) list of IDs of dictionaries.
+   */
+  public List<String> getDictionaryIds();
 
-   /**
-    * Gets the identified dictionary.
-    * <p> This method can assume that the following methods have been previously called:
-    * <ul>
-    *  <li> {@link Annotator#setSchema(Schema)} </li>
-    *  <li> {@link Annotator#setTaskParameters(String)} </li>
-    *  <li> {@link Annotator#setWorkingDirectory(File)} (if applicable) </li>
-    *  <li> {@link Annotator#setRdbConnectionFactory(ConnectionFactory)}
-    *       (if applicable) </li>
-    * </ul>
-    * @param id The indentifier of the desired dictionary. This can be null if the
-    * Annotator supports only one dictionary, or the Annotator has already been configured
-    * (e.g. via {@link Annotator#setTaskParameters(String)} sufficiently to know which
-    * dictionary to return.
-    * @return The identified dictionary.
-    * @throws DictionaryException If the given dictionary doesn't exist.
-    */
-   public Dictionary getDictionary(String id) throws DictionaryException;
+  /**
+   * Gets the identified dictionary.
+   * <p> This method can assume that the following methods have been previously called:
+   * <ul>
+   *  <li> {@link Annotator#setSchema(Schema)} </li>
+   *  <li> {@link Annotator#setTaskParameters(String)} </li>
+   *  <li> {@link Annotator#setWorkingDirectory(File)} (if applicable) </li>
+   *  <li> {@link Annotator#setRdbConnectionFactory(ConnectionFactory)}
+   *       (if applicable) </li>
+   * </ul>
+   * @param id The indentifier of the desired dictionary. This can be null if the
+   * Annotator supports only one dictionary, or the Annotator has already been configured
+   * (e.g. via {@link Annotator#setTaskParameters(String)} sufficiently to know which
+   * dictionary to return.
+   * @return The identified dictionary.
+   * @throws DictionaryException If the given dictionary doesn't exist.
+   */
+  public Dictionary getDictionary(String id) throws DictionaryException;
+  
+  /**
+   * Tags all instances of the given word in the given graph store, using the dictionary
+   * specified by current task configuration (i.e. the dictionary returned by
+   * <code>getDictionary(null)</code>).
+   * <p> The default implementation throws TransformationException
+   * @param store
+   * @param sourceLabel
+   * @return The number of tags created.
+   * @throws DictionaryException, TransformationException, InvalidConfigurationException,
+   * StoreException 
+   */
+  default public int tagAllInstances(GraphStore store, String sourceLabel)
+    throws DictionaryException, TransformationException, InvalidConfigurationException,
+    StoreException {
+    throw new TransformationException((GraphTransformer)this, "Not implemented");
+  }
    
-} // end of class UsesFileSystem
+} // end of class ImplementsDictionaries
