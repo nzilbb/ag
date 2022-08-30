2108db28f89eac6f42e60fdb682c81b9cf726dfa
Robert Fromont <robert@fromont.net.nz>
2022-08-30 14:26:20 -0300

    ReaperAnnotator: Make logging slightly less verbose, use empty string instead of null as media suffix.

--- annotator/reaper/src/main/java/nzilbb/annotator/reaper/ReaperAnnotator.java
+++ annotator/reaper/src/main/java/nzilbb/annotator/reaper/ReaperAnnotator.java
@@ -426,17 +426,37 @@ public class ReaperAnnotator extends Annotator {
             
             setStatus(transcript.getId() + " : Running reaper...");
             new Thread(reaper).start();
+            String lastCommand = null;
+            String lastCommandLine = null;
+            java.time.Instant lastStartInstant = null;
+            java.time.Duration lastTotalCpuDuration = null;
+            String lastUser = "";
             // wait until it's finished
             while (!reaper.getFinished()) {
               try { Thread.sleep(1000); } catch(Exception exception) {}
               if (reaper.getProcess() != null) { // TODO remove debug tracing
                 ProcessHandle.Info info = reaper.getProcess().info();
                 if (info != null) {
-                  setStatus("command: " + info.command());
-                  setStatus("commandLine: " + info.commandLine());
-                  setStatus("startInstant: " + info.startInstant());
-                  setStatus("totalCpuDuration: " + info.totalCpuDuration());
-                  setStatus("user: " + info.user());
+                  if (info.command().isPresent() && !info.command().get().equals(lastCommand)) {
+                    lastCommand = info.command().get();
+                    setStatus("command: " + lastCommand);
+                  }
+                  if (info.commandLine().isPresent() && !info.commandLine().get().equals(lastCommandLine)) {
+                    lastCommandLine = info.commandLine().get();
+                    setStatus("commandLine: " + lastCommandLine);
+                  }
+                  if (info.startInstant().isPresent() && !info.startInstant().get().equals(lastStartInstant)) {
+                    lastStartInstant = info.startInstant().get();
+                    setStatus("startInstant: " + lastStartInstant);
+                  }
+                  if (info.totalCpuDuration().isPresent() && !info.totalCpuDuration().get().equals(lastStartInstant)) {
+                    lastTotalCpuDuration = info.totalCpuDuration().get();
+                    setStatus("totalCpuDuration: " + lastTotalCpuDuration);
+                  }
+                  if (info.user().isPresent() && !info.user().get().equals(lastUser)) {
+                    lastUser = info.user().get();
+                    setStatus("user: " + lastUser);
+                  }
                 }
               }
             }
@@ -452,13 +472,14 @@ public class ReaperAnnotator extends Annotator {
             }
             
             if (f0.exists()) {              
-              setStatus("Saving " + f0.getName());
-              getStore().saveMedia(transcript.getId(), null, f0.toURI().toString());
+              setStatus("Saving " + f0.toURI().toString());
+              getStore().saveMedia(transcript.getId(), "", f0.toURI().toString());
               // remove temporary file
               f0.delete();
 
               // get back the file URL
               for (MediaFile media : transcript.getMediaProvider().getAvailableMedia()) {
+                setStatus("File: " + media.getName());
                 if (media.getExtension().equals("f0")
                     && media.getFile() != null
                     && media.getFile().exists()) {
@@ -469,7 +490,7 @@ public class ReaperAnnotator extends Annotator {
             } // .f0
             if (pm.exists()) {
               setStatus("Saving " + pm.getName());
-              getStore().saveMedia(transcript.getId(), null, pm.toURI().toString());
+              getStore().saveMedia(transcript.getId(), "", pm.toURI().toString());
               // remove temporary file
               pm.delete();
             } // .pm
@@ -479,6 +500,8 @@ public class ReaperAnnotator extends Annotator {
           }
         }
       } // has wav
+
+      setStatus("f0File: " + f0File);
 
       if (!isCancelling()
           && f0LayerId != null && f0File != null) { // parse .f0 file to generate annotations
