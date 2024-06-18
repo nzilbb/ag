b6f594047cdf322dec8923977805826cbcf3b669
Robert Fromont <robert@fromont.net.nz>
2024-06-18 15:47:05 -0300

    PatternTagger 2.0.6: fix help info icon on task configuration page.

--- annotator/patterntagger/src/main/java/nzilbb/annotator/patterntagger/PatternTagger.java
+++ annotator/patterntagger/src/main/java/nzilbb/annotator/patterntagger/PatternTagger.java
@@ -1,5 +1,5 @@
 //
-// Copyright 2020 New Zealand Institute of Language, Brain and Behaviour, 
+// Copyright 2020-2024 New Zealand Institute of Language, Brain and Behaviour, 
 // University of Canterbury
 // Written by Robert Fromont - robert.fromont@canterbury.ac.nz
 //
